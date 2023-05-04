0fe45e0746235e82449f02d7849712736346cf58
JhulianCode <aguilera.jhulian@gmail.com>
2023-05-04 05:28:55 -0300

    Se crear el Router

---
+++ Frontend/src/router/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom"
+import Home from "../components/pages/Home"
+import Error404 from "../components/pages/Error404"
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <Error404 />
+    }
+])
+
+export default Router
