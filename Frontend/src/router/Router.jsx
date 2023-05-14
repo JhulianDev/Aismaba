98707fd78b9198bacc61c6f61636eb5f4894a0a7
JhulianCode <aguilera.jhulian@gmail.com>
2023-05-14 07:45:49 -0300

    Se agrega el MainHeaderTemplate

--- Frontend/src/router/Router.jsx
+++ Frontend/src/router/Router.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom"
 import Home from "../components/pages/Home"
 import Error404 from "../components/pages/Error404"
+import MainHeaderTemplate from "../components/templates/MainHeaderTemplate"
 const Router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
-        errorElement: <Error404 />
+        element: <MainHeaderTemplate />,
+        errorElement: <Error404 />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
     }
 ])
 
