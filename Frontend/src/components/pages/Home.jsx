18d88f346635915b99866a8503482d0d068e70bf
JhulianCode <aguilera.jhulian@gmail.com>
2023-05-17 02:53:37 -0300

    Se crea la seccion Presentacion

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { ISOTIPO_BLANCO } from "../../assets/img/imagenes";
+import { ISOTIPO_BLANCO, FUNDADORA_1, FUNDADORA_2, ESCRITORIO_AISMABA } from "../../assets/img/imagenes";
 import { SOMBRAS_VIDEO } from "../../assets/vid/videos";
 
 const Home = () => {
@@ -40,6 +40,31 @@ const Home = () => {
               <br />
               <i>¡estamos aqui para crearlo!</i>
             </h3>
+          </div>
+
+          <div className="presentacion__box-imagenes">
+            <img src={FUNDADORA_1} alt="Foto de la creadora de Aismaba" className="presentacion__img-1" />
+            <img src={FUNDADORA_2} alt="Foto de la creadora de Aismaba" className="presentacion__img-2" />
+          </div>
+        </div>
+
+        <div className="presentacion__box-interrogante">
+          <img src={ESCRITORIO_AISMABA} alt="Foto del escritorio de Aismaba" className="presentacion__img-3" />
+
+          <div className="presentacion__box-textos">
+            <h2 className="presentacion__pregunta">¿COMO LO HAREMOS?</h2>
+            <p className="presentacion__respuesta">
+              Te brindaremos las herramientas necesarias para que tu emprendimiento sea un éxito en internet.
+              <br />
+              <br />
+              Diseñaremos paso a paso el branding de tu marca logrando que tus clientes puedan identificar cuales son sus valores, su visión y las cualidades que la hacen diferente a las demás.
+              <br />
+              <br />
+              Daremos vida a tus redes sociales ofreciendo contenido innovador, creativo y de calidad que te garantizara los mejores resultados.
+              <br />
+              <br />
+              No dejaremos nada a la suerte! Cada paso sera planificado a traves de nuestras estrategias de marketing para asegurarnos de que se cumplan todos tus objetivos!
+            </p>
           </div>
         </div>
 
