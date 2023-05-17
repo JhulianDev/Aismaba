a9db6b487b2777989b4424d712886ecdfc56a705
JhulianCode <aguilera.jhulian@gmail.com>
2023-05-17 02:52:20 -0300

    Se importan imagenes del componente Home

--- Frontend/src/assets/img/imagenes.js
+++ Frontend/src/assets/img/imagenes.js
@@ -1,5 +1,11 @@
 import isotipo from "./generales/logo_white.png";
 import carrito from "./generales/cart_white.png";
+import fundadora1 from "./generales/2.png";
+import fundadora2 from "./generales/3.jpg";
+import escritorio from "./generales/4.jpg"
 
 export const ISOTIPO_BLANCO = isotipo
 export const CARRITO_ICONO = carrito
+export const FUNDADORA_1 = fundadora1
+export const FUNDADORA_2 = fundadora2
+export const ESCRITORIO_AISMABA = escritorio
