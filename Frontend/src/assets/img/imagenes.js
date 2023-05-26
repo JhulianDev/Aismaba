e393ad0edf0aee5f25b977bad69c565802c9dd8d
JhulianCode <aguilera.jhulian@gmail.com>
2023-05-26 06:12:20 -0300

    Se importan imagenes utilizadas en el componente Home.jsx

--- Frontend/src/assets/img/imagenes.js
+++ Frontend/src/assets/img/imagenes.js
@@ -3,9 +3,17 @@ import carrito from "./generales/cart_white.png";
 import fundadora1 from "./generales/2.png";
 import fundadora2 from "./generales/3.jpg";
 import escritorio from "./generales/4.jpg"
+import comillas from "./generales/comillas.png";
+import productosDigitales from "./generales/mockup-productos-digitales.png";
+import nuestrosServicios from "./generales/mockup-nuestros-servicios.png";
+import recursosGratuitos from "./generales/mockup-resursos-gratuitos.png";
 
 export const ISOTIPO_BLANCO = isotipo
 export const CARRITO_ICONO = carrito
 export const FUNDADORA_1 = fundadora1
 export const FUNDADORA_2 = fundadora2
 export const ESCRITORIO_AISMABA = escritorio
+export const COMILLAS_GRISES = comillas
+export const PRODUCTOS_DIGITALES = productosDigitales
+export const NUESTROS_SERVICIOS = nuestrosServicios
+export const RECURSOS_GRATUITOS = recursosGratuitos
