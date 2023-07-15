7ac34a909a012057d9ba9a7cfbdbcb4c1ea06b3c
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-15 05:04:56 -0300

    Se añade paleta de colores

---
+++ Frontend/src/assets/css/Colors.js
@@ -0,0 +1,10 @@
+export const colores = {
+  colorAcento:      "#9f7253",
+  colorPrincipal:   "#bf9b7a",
+  colorSecundario:  "#d9cdbf",
+  colorTerciarios:  "#e4d9c5",
+  colorTitulo:      "#2b2b2b",
+  colorTexto:       "#939393",
+  colorFondo:       "#ece8e3",
+  colorBlanco:      "#ffffff",
+};
