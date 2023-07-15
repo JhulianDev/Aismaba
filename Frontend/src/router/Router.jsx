76e18bdd297e9a78e81c645b0ba1e4aae45d267b
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-15 04:45:11 -0300

    Se creo y se agrego configuracion inicial al Router

---
+++ Frontend/src/router/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import HeaderTemplate from "../components/templates/HeaderTemplate";
+import Error404 from "../components/pages/Error404";
+import Home from "../components/pages/Home";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HeaderTemplate />,
+    errorElement: <Error404 />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
+
+export default Router
