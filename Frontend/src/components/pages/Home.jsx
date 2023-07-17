37c7d74317eeea22a3b916f07cd0d9ace8b3397c
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-17 05:44:35 -0300

    Se agrega el componente Presentacion a la pagina Home

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import HamburgerButton from '../general/Buttons/hamburger/HamburgerButton';
 import Portada from './home/portada/Portada';
+import Presentacion from './home/presentacion/Presentacion';
 
 const Home = () => {
   return (
-    <Portada/>
+    <>
+      <Portada />
+      <Presentacion />
+    </>
   );
 };
 
