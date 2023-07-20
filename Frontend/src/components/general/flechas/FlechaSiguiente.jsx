611d92507805b621be8690bbf72eae7183f0839e
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-20 05:58:18 -0300

    Se crean los componentes Flechas

---
+++ Frontend/src/components/general/flechas/FlechaSiguiente.jsx
@@ -0,0 +1,18 @@
+import { FlechaContainer, FlechaSvg } from "./FlechaStyled";
+
+const FlechaSiguiente = () => {
+  return (
+    <FlechaContainer>
+      <FlechaSvg
+        xmlns="http://www.w3.org/2000/svg"
+        height="48"
+        width="48"
+        viewBox="0 0 48 48"
+        fill="currentcolor">
+        <path d="m18.75 36-2.15-2.15 9.9-9.9-9.9-9.9 2.15-2.15L30.8 23.95Z"/>
+      </FlechaSvg>
+    </FlechaContainer>
+  );
+};
+
+export default FlechaSiguiente;
