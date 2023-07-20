e3ba27a1236f3ba4cd840dc9bcafb87dc88d635f
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-20 04:37:46 -0300

    Se elimina importacion css

--- Frontend/src/components/pages/home/pensamiento/PensamientoStyled.jsx
+++ Frontend/src/components/pages/home/pensamiento/PensamientoStyled.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const PensamientoSection = styled.section `
   background-image: url(../../../src/assets/img/generales/5b.jpg);
