92586d1469fdd31cf137fb2519dcd576ead083cf
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-20 04:20:09 -0300

    Se agrego el componente Pensamiento a la pagina Home

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HamburgerButton from '../general/Buttons/hamburger/HamburgerButton';
 import Portada from './home/portada/Portada';
 import Presentacion from './home/presentacion/Presentacion';
+import Pensamiento from './home/pensamiento/Pensamiento';
 
 const Home = () => {
   return (
     <>
       <Portada />
       <Presentacion />
+      <Pensamiento />
     </>
   );
 };
