1d4c5c617611e9f766aba4210a63a8adbd260d15
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-21 05:09:44 -0300

    Se modifico nombre de color

--- Frontend/src/assets/css/Colors.js
+++ Frontend/src/assets/css/Colors.js
@@ -2,7 +2,7 @@ export const colores = {
   colorAcento:      "#9f7253",
   colorPrincipal:   "#bf9b7a",
   colorSecundario:  "#d9cdbf",
-  colorTerciarios:  "#e4d9c5",
+  colorTerciario:  "#e4d9c5",
   colorTitulo:      "#2b2b2b",
   colorTexto:       "#939393",
   colorFondo:       "#ece8e3",
