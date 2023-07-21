acfa0bfe16100f2d335011e3e7b34e867177cd07
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-21 05:20:51 -0300

    Se agrega el componente Opciones a la pagina Home

---
+++ Frontend/src/components/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import Portada from './portada/Portada';
+import Presentacion from './presentacion/Presentacion';
+import Pensamiento from './pensamiento/Pensamiento';
+import Opciones from './opciones/Opciones';
+
+const Home = () => {
+  return (
+    <>
+      <Portada />
+      <Presentacion />
+      <Pensamiento />
+      <Opciones />
+    </>
+  );
+};
+
+export default Home;
