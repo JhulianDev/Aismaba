7b74e6788b0b87a1a898447e6a16afaa8181bf6a
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-21 05:20:15 -0300

    Se cambia y se actualiza la ubicacion de la pagina Home

--- Frontend/src/router/Router.jsx
+++ Frontend/src/router/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HeaderTemplate from "../components/templates/HeaderTemplate";
-import Home from "../components/pages/Home";
 import Error404 from "../components/pages/Error404/Error404";
+import Home from "../components/pages/home/Home";
 
 const Router = createBrowserRouter([
   {
