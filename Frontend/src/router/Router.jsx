870eea2a8c722140129f6a20df6221d56f9b4970
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-22 04:34:14 -0300

    Se agrega la ruta /servicios

--- Frontend/src/router/Router.jsx
+++ Frontend/src/router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HeaderTemplate from "../components/templates/HeaderTemplate";
 import Error404 from "../components/pages/Error404/Error404";
 import Home from "../components/pages/home/Home";
+import Servicios from "../components/pages/servicios/Servicios";
 
 const Router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const Router = createBrowserRouter([
       {
         index: true,
         element: <Home />
+      },
+      {
+        path: "/servicios",
+        element: <Servicios />
       }
     ]
   }
