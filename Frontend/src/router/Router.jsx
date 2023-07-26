a50b9a27d22bbbde5c497dfa95dd30923d977f73
JhulianCode <aguilera.jhulian@gmail.com>
2023-07-25 23:41:14 -0300

    Se agrega ruta a pagina de Branding

--- Frontend/src/router/Router.jsx
+++ Frontend/src/router/Router.jsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import HeaderTemplate from "../components/templates/HeaderTemplate";
+import Header from "../components/general/header/Header";
 import Error404 from "../components/pages/Error404/Error404";
 import Home from "../components/pages/home/Home";
 import Servicios from "../components/pages/servicios/Servicios";
+import Branding from "../components/pages/branding/Branding";
+import { colores } from "../assets/css/Colors";
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <HeaderTemplate />,
+    element: <Header backgroundColor={colores.colorPrincipal} />,
     errorElement: <Error404 />,
     children: [
       {
@@ -17,6 +19,15 @@ const Router = createBrowserRouter([
       {
         path: "/servicios",
         element: <Servicios />
+      }
+    ]
+  },
+  {
+    element: <Header backgroundColor={"transparent"} />,
+    children: [
+      {
+        path: "/branding",
+        element: <Branding />
       }
     ]
   }
