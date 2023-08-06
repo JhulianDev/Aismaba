3872442814d06166d79a4dfe4d8473826141b880
JhulianCode <aguilera.jhulian@gmail.com>
2023-08-06 06:25:31 -0300

    Se agrega el componente Categorias a la pagina de Tienda

--- Frontend/src/components/pages/tienda/Tienda.jsx
+++ Frontend/src/components/pages/tienda/Tienda.jsx
@@ -1,13 +1,23 @@
-import { PORTADA_TIENDA } from "../../../assets/img/images";
+import { CATEGORIA_MARCAS, CATEGORIA_PLANTILLAS, CATEGORIA_PRESETS, PORTADA_TIENDA } from "../../../assets/img/images";
 import Cabecera from "../../general/cabecera/Cabecera";
+import Separador from "../../general/separador/Separador";
+import Categorias from "./categorias/Categorias";
 
 const Tienda = () => {
   return (
-    <Cabecera 
-    imagen={PORTADA_TIENDA}
-    tituloS="Productos Digitales"
-    parrafo="Ahorre horas de edición con nuestros productos y dedique más tiempo a conectarse con su audiencia"
-    />
+    <>
+      <Cabecera
+        imagen={PORTADA_TIENDA}
+        tituloS="Productos Digitales"
+        parrafo="Ahorre horas de edición con nuestros productos y dedique más tiempo a conectarse con su audiencia"
+      />
+
+      <Separador titulo="NUESTRAS CATEGORIAS" />
+
+      <Categorias />
+
+      <Separador titulo="PLANTILLAS INSTAGRAM" />
+    </>
   );
 };
 
