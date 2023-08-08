611255355b822d6bb6603941ab9cbf386970509c
JhulianCode <aguilera.jhulian@gmail.com>
2023-08-08 05:34:33 -0300

    Se crea contexto para la pagina de Tienda

---
+++ Frontend/src/context/ShopContext.jsx
@@ -0,0 +1,33 @@
+import { createContext } from "react";
+import { AESTHETIC_BROWN_1, CATEGORIA_MARCAS, CATEGORIA_PLANTILLAS, CATEGORIA_PRESETS, CURVE_PINK_1, ELEGANT_INSPO_1, GREEN_WORLD_1, MINIMAL_MOOD_1, MIX_NUDE_1, OCEAN_BLUE_1, ROSE_LOVE_1, SOL_GREY_1 } from "../assets/img/images";
+
+const shopData = {
+  categorias: [
+    { categoria : "Plantillas", tipo: "Instagram",      alt : "Mockup Plantillas Instagram",  imagen: CATEGORIA_PLANTILLAS },
+    { categoria : "Marcas",     tipo: "Personales",     alt : "Mockup Brandboard",            imagen: CATEGORIA_MARCAS },
+    { categoria : "Presets",    tipo: "Lightroom",      alt : "Mockup Presets Lightroom",     imagen: CATEGORIA_PRESETS }
+  ],
+  plantillas: [
+    { nombre : "Minimal Mood",    tipo: "Plantillas Instagram", precio: "20", imagen: MINIMAL_MOOD_1 },
+    { nombre : "Ocean Blue",      tipo: "Plantillas Instagram", precio: "20", imagen: OCEAN_BLUE_1 },
+    { nombre : "Elegant Inspo",   tipo: "Plantillas Instagram", precio: "20", imagen: ELEGANT_INSPO_1 },
+    { nombre : "Green World",     tipo: "Plantillas Instagram", precio: "20", imagen: GREEN_WORLD_1 },
+    { nombre : "Curve Pink",      tipo: "Plantillas Instagram", precio: "20", imagen: CURVE_PINK_1 },
+    { nombre : "Aesthetic Brown", tipo: "Plantillas Instagram", precio: "20", imagen: AESTHETIC_BROWN_1 },
+    { nombre : "Sol Grey",        tipo: "Plantillas Instagram", precio: "20", imagen: SOL_GREY_1 },
+    { nombre : "Rose Love",       tipo: "Plantillas Instagram", precio: "20", imagen: ROSE_LOVE_1 },
+    { nombre : "Mix Nude",        tipo: "Plantillas Instagram", precio: "20", imagen: MIX_NUDE_1 }
+  ]
+}
+
+const ShopContext = createContext()
+
+const ShopProvider = ({children}) => {
+  return (
+    <ShopContext.Provider value={ shopData }>
+      {children}
+    </ShopContext.Provider>
+  )
+}
+
+export { ShopContext, ShopProvider }
