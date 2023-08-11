c957ae782ef423bc4d4d94f8af4af232d9ae2d6a
JhulianCode <aguilera.jhulian@gmail.com>
2023-08-10 21:25:14 -0300

    Se agrega contenido al shopData

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { AESTHETIC_BROWN_1, CATEGORIA_MARCAS, CATEGORIA_PLANTILLAS, CATEGORIA_PRESETS, CURVE_PINK_1, ELEGANT_INSPO_1, GREEN_WORLD_1, MINIMAL_MOOD_1, MIX_NUDE_1, OCEAN_BLUE_1, ROSE_LOVE_1, SOL_GREY_1 } from "../assets/img/images";
+import { AESTHETIC_BROWN_1, CATEGORIA_MARCAS, CATEGORIA_PLANTILLAS, CATEGORIA_PRESETS, CURVE_PINK_1, ELEGANT_INSPO_1, GREEN_WORLD_1, MINIMAL_MOOD_1, MIX_NUDE_1, OCEAN_BLUE_1, PORTADA_ALMA_STUDIO, ROSE_LOVE_1, SOL_GREY_1 } from "../assets/img/images";
 
 const shopData = {
   categorias: [
@@ -17,6 +17,14 @@ const shopData = {
     { nombre : "Sol Grey",        tipo: "Plantillas Instagram", precio: "20", imagen: SOL_GREY_1 },
     { nombre : "Rose Love",       tipo: "Plantillas Instagram", precio: "20", imagen: ROSE_LOVE_1 },
     { nombre : "Mix Nude",        tipo: "Plantillas Instagram", precio: "20", imagen: MIX_NUDE_1 }
+  ],
+  marcas: [
+    { 
+      nombre : "Alma Studio", 
+      descripcion: "Esta identidad prediseñada es perfecta para aportar una imagen fresca y elegante a tu proyecto. Su diseño tipográfico estilizado le otorga a tu marca un hermoso toque moderno y minimalista.", 
+      precio: "100", 
+      imagen: PORTADA_ALMA_STUDIO
+    }
   ]
 }
 
