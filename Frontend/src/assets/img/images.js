16dee8b4979a613051cff26d10cadbc21ab47d17
JhulianCode <aguilera.jhulian@gmail.com>
2023-08-17 03:48:30 -0300

    Se importan nuevas imagenes y se crean arreglos para imagenes de productos

--- Frontend/src/assets/img/images.js
+++ Frontend/src/assets/img/images.js
@@ -47,13 +47,223 @@ export { default as PORTADA_TIENDA } from "./generales/portada-tienda.png";
 export { default as CATEGORIA_PLANTILLAS } from "./generales/categoria-plantillas.jpg";
 export { default as CATEGORIA_MARCAS } from "./generales/categoria-marcas.jpg";
 export { default as CATEGORIA_PRESETS } from "./generales/categoria-presets.jpg";
+
+// Plantillas Minimal Moood
 export { default as MINIMAL_MOOD_1 } from "./plantillas/1 MINIMAL MOOD/1. MINIMAL MOOD.png";
+import MINIMAL_MOOD_1 from "./plantillas/1 MINIMAL MOOD/1. MINIMAL MOOD.png";
+import MINIMAL_MOOD_2 from "./plantillas/1 MINIMAL MOOD/2. MINIMAL MOOD.png";
+import MINIMAL_MOOD_3 from "./plantillas/1 MINIMAL MOOD/3. MINIMAL MOOD.png";
+import MINIMAL_MOOD_4 from "./plantillas/1 MINIMAL MOOD/4. MINIMAL MOOD.png";
+import MINIMAL_MOOD_5 from "./plantillas/1 MINIMAL MOOD/5. MINIMAL MOOD.png";
+import MINIMAL_MOOD_6 from "./plantillas/1 MINIMAL MOOD/6. MINIMAL MOOD.png";
+import MINIMAL_MOOD_7 from "./plantillas/1 MINIMAL MOOD/7. MINIMAL MOOD.png";
+import MINIMAL_MOOD_8 from "./plantillas/1 MINIMAL MOOD/8. MINIMAL MOOD.png";
+
+export const minimalMoodImages = [
+  MINIMAL_MOOD_1,
+  MINIMAL_MOOD_2,
+  MINIMAL_MOOD_3,
+  MINIMAL_MOOD_4,
+  MINIMAL_MOOD_5,
+  MINIMAL_MOOD_6,
+  MINIMAL_MOOD_7,
+  MINIMAL_MOOD_8
+];
+
+// Plantillas Ocean Blue
 export { default as OCEAN_BLUE_1 } from "./plantillas/4 OCEAN BLUE/1. OCEAN BLUE.png";
+import OCEAN_BLUE_1 from "./plantillas/4 OCEAN BLUE/1. OCEAN BLUE.png";
+import OCEAN_BLUE_2 from "./plantillas/4 OCEAN BLUE/2. OCEAN BLUE.png";
+import OCEAN_BLUE_3 from "./plantillas/4 OCEAN BLUE/3. OCEAN BLUE.png";
+import OCEAN_BLUE_4 from "./plantillas/4 OCEAN BLUE/4. OCEAN BLUE.png";
+import OCEAN_BLUE_5 from "./plantillas/4 OCEAN BLUE/5. OCEAN BLUE.png";
+import OCEAN_BLUE_6 from "./plantillas/4 OCEAN BLUE/6. OCEAN BLUE.png";
+import OCEAN_BLUE_7 from "./plantillas/4 OCEAN BLUE/7. OCEAN BLUE.png";
+import OCEAN_BLUE_8 from "./plantillas/4 OCEAN BLUE/8. OCEAN BLUE.png";
+
+export const oceanBlueImages = [
+  OCEAN_BLUE_1,
+  OCEAN_BLUE_2,
+  OCEAN_BLUE_3,
+  OCEAN_BLUE_4,
+  OCEAN_BLUE_5,
+  OCEAN_BLUE_6,
+  OCEAN_BLUE_7,
+  OCEAN_BLUE_8
+];
+
+// Plantillas Elegant Inspo
 export { default as ELEGANT_INSPO_1 } from "./plantillas/2 ELEGANT INSPO/1. ELEGANT INSPO.png";
+import ELEGANT_INSPO_1 from "./plantillas/2 ELEGANT INSPO/1. ELEGANT INSPO.png";
+import ELEGANT_INSPO_2 from "./plantillas/2 ELEGANT INSPO/2. ELEGANT INSPO.png";
+import ELEGANT_INSPO_3 from "./plantillas/2 ELEGANT INSPO/3. ELEGANT INSPO.png";
+import ELEGANT_INSPO_4 from "./plantillas/2 ELEGANT INSPO/4. ELEGANT INSPO.png";
+import ELEGANT_INSPO_5 from "./plantillas/2 ELEGANT INSPO/5. ELEGANT INSPO.png";
+import ELEGANT_INSPO_6 from "./plantillas/2 ELEGANT INSPO/6. ELEGANT INSPO.png";
+import ELEGANT_INSPO_7 from "./plantillas/2 ELEGANT INSPO/7. ELEGANT INSPO.png";
+import ELEGANT_INSPO_8 from "./plantillas/2 ELEGANT INSPO/8. ELEGANT INSPO.png";
+
+export const elegantInspoImages = [
+  ELEGANT_INSPO_1,
+  ELEGANT_INSPO_2,
+  ELEGANT_INSPO_3,
+  ELEGANT_INSPO_4,
+  ELEGANT_INSPO_5,
+  ELEGANT_INSPO_6,
+  ELEGANT_INSPO_7,
+  ELEGANT_INSPO_8
+];
+
+// Plantillas Green World
 export { default as GREEN_WORLD_1 } from "./plantillas/5 GREEN WORLD/1. GREEN WORLD.png";
+import GREEN_WORLD_1 from "./plantillas/5 GREEN WORLD/1. GREEN WORLD.png";
+import GREEN_WORLD_2 from "./plantillas/5 GREEN WORLD/2. GREEN WORLD.png";
+import GREEN_WORLD_3 from "./plantillas/5 GREEN WORLD/3. GREEN WORLD.png";
+import GREEN_WORLD_4 from "./plantillas/5 GREEN WORLD/4. GREEN WORLD.png";
+import GREEN_WORLD_5 from "./plantillas/5 GREEN WORLD/5. GREEN WORLD.png";
+import GREEN_WORLD_6 from "./plantillas/5 GREEN WORLD/6. GREEN WORLD.png";
+import GREEN_WORLD_7 from "./plantillas/5 GREEN WORLD/7. GREEN WORLD.png";
+import GREEN_WORLD_8 from "./plantillas/5 GREEN WORLD/8. GREEN WORLD.png";
+
+export const greenWorldImages = [
+  GREEN_WORLD_1,
+  GREEN_WORLD_2,
+  GREEN_WORLD_3,
+  GREEN_WORLD_4,
+  GREEN_WORLD_5,
+  GREEN_WORLD_6,
+  GREEN_WORLD_7,
+  GREEN_WORLD_8
+];
+
+// Plantillas Curve Pink
 export { default as CURVE_PINK_1 } from "./plantillas/3 CURVE PINK/1. CURVE PINK.png";
+import CURVE_PINK_1 from "./plantillas/3 CURVE PINK/1. CURVE PINK.png";
+import CURVE_PINK_2 from "./plantillas/3 CURVE PINK/2. CURVE PINK.png";
+import CURVE_PINK_3 from "./plantillas/3 CURVE PINK/3. CURVE PINK.png";
+import CURVE_PINK_4 from "./plantillas/3 CURVE PINK/4. CURVE PINK.png";
+import CURVE_PINK_5 from "./plantillas/3 CURVE PINK/5. CURVE PINK.png";
+import CURVE_PINK_6 from "./plantillas/3 CURVE PINK/6. CURVE PINK.png";
+import CURVE_PINK_7 from "./plantillas/3 CURVE PINK/7. CURVE PINK.png";
+import CURVE_PINK_8 from "./plantillas/3 CURVE PINK/8. CURVE PINK.png";
+
+export const curvePinkImages = [
+  CURVE_PINK_1,
+  CURVE_PINK_2,
+  CURVE_PINK_3,
+  CURVE_PINK_4,
+  CURVE_PINK_5,
+  CURVE_PINK_6,
+  CURVE_PINK_7,
+  CURVE_PINK_8
+];
+
+// Plantillas Aesthetic Brows
 export { default as AESTHETIC_BROWN_1 } from "./plantillas/7 AESTHETIC BROWN/1. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_1 from "./plantillas/7 AESTHETIC BROWN/1. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_2 from "./plantillas/7 AESTHETIC BROWN/2. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_3 from "./plantillas/7 AESTHETIC BROWN/3. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_4 from "./plantillas/7 AESTHETIC BROWN/4. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_5 from "./plantillas/7 AESTHETIC BROWN/5. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_6 from "./plantillas/7 AESTHETIC BROWN/6. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_7 from "./plantillas/7 AESTHETIC BROWN/7. AESTHETIC BROWN.png";
+import AESTHETIC_BROWN_8 from "./plantillas/7 AESTHETIC BROWN/8. AESTHETIC BROWN.png";
+
+export const aestheticBrownImages = [
+  AESTHETIC_BROWN_1,
+  AESTHETIC_BROWN_2,
+  AESTHETIC_BROWN_3,
+  AESTHETIC_BROWN_4,
+  AESTHETIC_BROWN_5,
+  AESTHETIC_BROWN_6,
+  AESTHETIC_BROWN_7,
+  AESTHETIC_BROWN_8
+];
+
+// Plantillas Sol Grey
 export { default as SOL_GREY_1 } from "./plantillas/6 SOL GREY/1. SOL GREY.png";
+import SOL_GREY_1 from "./plantillas/6 SOL GREY/1. SOL GREY.png";
+import SOL_GREY_2 from "./plantillas/6 SOL GREY/2. SOL GREY.png";
+import SOL_GREY_3 from "./plantillas/6 SOL GREY/3. SOL GREY.png";
+import SOL_GREY_4 from "./plantillas/6 SOL GREY/4. SOL GREY.png";
+import SOL_GREY_5 from "./plantillas/6 SOL GREY/5. SOL GREY.png";
+import SOL_GREY_6 from "./plantillas/6 SOL GREY/6. SOL GREY.png";
+import SOL_GREY_7 from "./plantillas/6 SOL GREY/7. SOL GREY.png";
+import SOL_GREY_8 from "./plantillas/6 SOL GREY/8. SOL GREY.png";
+
+export const solGreyImages = [
+  SOL_GREY_1,
+  SOL_GREY_2,
+  SOL_GREY_3,
+  SOL_GREY_4,
+  SOL_GREY_5,
+  SOL_GREY_6,
+  SOL_GREY_7,
+  SOL_GREY_8
+];
+
+// Plantillas Rose Love
 export { default as ROSE_LOVE_1 } from "./plantillas/8 ROSE LOVE/1. ROSE LOVE.png";
+import ROSE_LOVE_1 from "./plantillas/8 ROSE LOVE/1. ROSE LOVE.png";
+import ROSE_LOVE_2 from "./plantillas/8 ROSE LOVE/2. ROSE LOVE.png";
+import ROSE_LOVE_3 from "./plantillas/8 ROSE LOVE/3. ROSE LOVE.png";
+import ROSE_LOVE_4 from "./plantillas/8 ROSE LOVE/4. ROSE LOVE.png";
+import ROSE_LOVE_5 from "./plantillas/8 ROSE LOVE/5. ROSE LOVE.png";
+import ROSE_LOVE_6 from "./plantillas/8 ROSE LOVE/6. ROSE LOVE.png";
+import ROSE_LOVE_7 from "./plantillas/8 ROSE LOVE/7. ROSE LOVE.png";
+import ROSE_LOVE_8 from "./plantillas/8 ROSE LOVE/8. ROSE LOVE.png";
+
+export const roseLoveImages = [
+  ROSE_LOVE_1,
+  ROSE_LOVE_2,
+  ROSE_LOVE_3,
+  ROSE_LOVE_4,
+  ROSE_LOVE_5,
+  ROSE_LOVE_6,
+  ROSE_LOVE_7,
+  ROSE_LOVE_8
+];
+
+// Plantillas Mix Nude
 export { default as MIX_NUDE_1 } from "./plantillas/9 MIX NUDE/1. MIX NUDE.png";
+import MIX_NUDE_1 from "./plantillas/9 MIX NUDE/1. MIX NUDE.png";
+import MIX_NUDE_2 from "./plantillas/9 MIX NUDE/2. MIX NUDE.png";
+import MIX_NUDE_3 from "./plantillas/9 MIX NUDE/3. MIX NUDE.png";
+import MIX_NUDE_4 from "./plantillas/9 MIX NUDE/4. MIX NUDE.png";
+import MIX_NUDE_5 from "./plantillas/9 MIX NUDE/5. MIX NUDE.png";
+import MIX_NUDE_6 from "./plantillas/9 MIX NUDE/6. MIX NUDE.png";
+import MIX_NUDE_7 from "./plantillas/9 MIX NUDE/7. MIX NUDE.png";
+import MIX_NUDE_8 from "./plantillas/9 MIX NUDE/8. MIX NUDE.png";
+
+export const mixNudeImages = [
+  MIX_NUDE_1,
+  MIX_NUDE_2,
+  MIX_NUDE_3,
+  MIX_NUDE_4,
+  MIX_NUDE_5,
+  MIX_NUDE_6,
+  MIX_NUDE_7,
+  MIX_NUDE_8
+];
+
+// Marca Alma Studio
 export { default as PORTADA_ALMA_STUDIO } from "./marcas/alma/portada-alma.png";
+import PORTADA_ALMA_STUDIO from "./marcas/alma/portada-alma.png";
+import ALMA_LOGO_PRINCIPAL from "./marcas/alma/logo-principal-alma.png";
+import ALMA_LOGO_SECUNDARIO from "./marcas/alma/logo-secundario-alma.png";
+import ALMA_PALETA_COLORES from "./marcas/alma/paleta-de-colores-alma.png";
+import ALMA_PORTADAS_HISTORIAS from "./marcas/alma/portadas-historias-alma.png";
+import ALMA_TARJETA_PERSONAL from "./marcas/alma/tarjeta-personal-alma.png";
+import ALMA_VARIANTE_1 from "./marcas/alma/variante-1-alma.png";
+import ALMA_VARIANTE_2 from "./marcas/alma/variante-2-alma.png";
+
+export const almaStudioImages = {
+  portada: PORTADA_ALMA_STUDIO,
+  logoPrincipal: ALMA_LOGO_PRINCIPAL,
+  logoSecundario: ALMA_LOGO_SECUNDARIO,
+  paletaColores: ALMA_PALETA_COLORES,
+  portadasHistorias: ALMA_PORTADAS_HISTORIAS,
+  tarjetaPersonal: ALMA_TARJETA_PERSONAL,
+  variante1: ALMA_VARIANTE_1,
+  variante2: ALMA_VARIANTE_2
+};
