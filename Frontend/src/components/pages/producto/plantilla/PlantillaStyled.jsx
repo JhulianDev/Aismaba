84cc2fa9e61a2ed9fbad96d95280cde11e857355
JhulianCode <aguilera.jhulian@gmail.com>
2023-08-20 05:57:38 -0300

    Se corrigen estilos

--- Frontend/src/components/pages/producto/plantilla/PlantillaStyled.jsx
+++ Frontend/src/components/pages/producto/plantilla/PlantillaStyled.jsx
@@ -15,58 +15,6 @@ export const PlantillaSection = styled.section`
   overflow: hidden;
 
   @media screen and (max-width: 768px){
-    padding: 120px 20px 60px;
-  }
-`
-
-export const PlantillaBoxInfo = styled.div`
-  max-width: 1200px;
-  display: flex;
-  flex-direction: column;
-
-  @media screen and (max-width: 1440px){
-    max-width: 934px;
-  }
-`
-
-export const PlantillaInfoTitulo = styled.h1`
-  font-family: Cinzel;
-  font-size: 1.5rem;
-  margin-bottom: 10px;
-
-  @media screen and (max-width: 768px){
-    text-align: center;
-  }
-`
-
-export const PlantillaInfoParrafo = styled.p`
-  font-family: Roboto;
-  font-size: 1rem;
-  line-height: 1.6rem;
-  text-align: justify;
-
-  @media screen and (max-width: 425px){
-    font-size: 1.1rem;
-  }
-`
-
-export const PlantillaTerminos = styled.p`
-  background-color: ${colores.colorTerciario};
-  max-width: 1200px;
-  font-family: Roboto;
-  font-size: 1rem;
-  line-height: 1.4rem;
-  text-align: center;
-  padding: 20px 200px;
-  margin-top: 10px;
-  border: double 4px ${colores.colorAcento};
-
-  @media screen and (max-width: 1440px){
-    max-width: 934px;
-    padding: 20px 70px;
-  }
-
-  @media screen and (max-width: 768px){
-    padding: 20px 40px;
+    padding: 120px 20px 40px;
   }
 `
