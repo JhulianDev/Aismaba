0ba98984bd665e2d9760cf7d897f9aeaceae470c
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-07 05:58:58 -0300

    Se envuelve la aplicacion en el contexto de usuario

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,13 +5,16 @@ import "./assets/css/normalize.css"
 import { ShopProvider } from './context/ShopContext'
 import { CartProvider } from './context/CartContext'
 import { CurrencyProvider } from './context/CurrencyContext'
+import { UserProvider } from './context/UserContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <CurrencyProvider>
-    <CartProvider>
-      <ShopProvider>
-        <RouterProvider router={Router} />
-      </ShopProvider>
-    </CartProvider>
-  </CurrencyProvider>
+  <UserProvider>
+    <CurrencyProvider>
+      <CartProvider>
+        <ShopProvider>
+          <RouterProvider router={Router} />
+        </ShopProvider>
+      </CartProvider>
+    </CurrencyProvider>
+  </UserProvider>
 )
