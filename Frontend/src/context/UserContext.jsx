43771c508a798880982c39f73d1475b17a94b367
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-07 05:58:32 -0300

    Se crea contexto para informacion del usuario

---
+++ Frontend/src/context/UserContext.jsx
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { createContext, useEffect, useState } from "react"
+import { getToken } from "../helpers/token"
+import { API_URL } from "../env/env"
+
+const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+
+  const [userData, setUserData] = useState()
+
+  useEffect(() => {
+    if (getToken()) {
+      axios.get(`${API_URL}/user`, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`
+        }
+      })
+        .then((resp) => {
+          setUserData(resp.data.user)
+        })
+        .catch((error) => {
+          console.log(`Error al obtener el usuario: ${error}`);
+        });
+    }
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export { UserContext, UserProvider }
