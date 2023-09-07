be698f1b4f68822ed707650cf3b5d7656abc3d89
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-07 04:20:02 -0300

    Se implementa el uso de Cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from "cors";
 import router from './routes/router.js';
 import { dataBaseConnection } from './database/database.js';
 const PORT = process.env.PORT || 3000
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/", router)
