992df3fe3817c209cc2d018e217430226548636d
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-08 06:09:57 -0300

    Se agregan nuevas funcionalidades al header y se reestructuran sus estilos

--- Frontend/src/components/general/header/Header.jsx
+++ Frontend/src/components/general/header/Header.jsx
@@ -1,39 +1,97 @@
-import { useState } from "react";
-import { Outlet } from 'react-router-dom';
-import { CARRITO_ICONO, ISOTIPO_BLANCO } from "../../../assets/img/images";
+import { useContext, useEffect, useState } from "react";
+import { Outlet, useNavigate } from 'react-router-dom';
+import Swal from "sweetalert2"
+import { UserContext } from "../../../context/UserContext";
 import HamburgerButton from "../Buttons/hamburger/HamburgerButton";
-import { CartContainer, CartIcon, HeaderContainer, HeaderLi, HeaderLink, HeaderNav, HeaderUl, LogoContainer, LogoImage } from "./HeaderStyled";
+import { CARRITO_ICONO, ISOTIPO_BLANCO, LOGOUT_ICON } from "../../../assets/img/images";
+import { BoxContainer, BoxCounter, CartContainer, CartIcon, HeaderBox, HeaderButton, HeaderContainer, HeaderLi, HeaderLink, HeaderNav, HeaderUl, LogoContainer, LogoImage, ProductCounter, Span } from "./HeaderStyled";
+import { deleteToken } from "../../../helpers/token";
+import { CartContext } from "../../../context/CartContext";
 
 const Header = ({ color }) => {
+  const { userData, setUserData } = useContext(UserContext)
+  const { state } = useContext(CartContext)
+  const [scrolling, setScrolling] = useState(false);
   const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
   const handleNav = () => {
     setOpen(!open);
   };
 
+  const handleSession = () => {
+    Swal.fire({
+      icon: "success",
+      title: "Hasta Luego",
+      text: "¡Vuelve Pronto!",
+      showConfirmButton: false,
+      timer: 1600
+    })
+
+    deleteToken()
+    setUserData(null)
+    navigate("/")
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <HeaderContainer $backgroundColor={color}>
+      <HeaderContainer $scrolling={scrolling} $backgroundColor={color}>
+        <HeaderBox>
 
-        <LogoContainer>
-          <LogoImage src={ISOTIPO_BLANCO} alt="Isotipo de Aismaba" />
-        </LogoContainer>
+          <HamburgerButton open={open} handleNav={handleNav} />
 
-        <HamburgerButton open={open} handleNav={handleNav} />
+          <LogoContainer>
+            <LogoImage src={ISOTIPO_BLANCO} alt="Isotipo de Aismaba" />
+          </LogoContainer>
 
-        <HeaderNav open={open} >
-          <HeaderUl>
-            <HeaderLi><HeaderLink to="/" onClick={handleNav}>Inicio</HeaderLink></HeaderLi>
-            <HeaderLi><HeaderLink to="/servicios" onClick={handleNav}>Servicios</HeaderLink></HeaderLi>
-            <HeaderLi><HeaderLink to="/Tienda" onClick={handleNav}>Tienda</HeaderLink></HeaderLi>
-            <HeaderLi><HeaderLink to="/Contacto" onClick={handleNav}>Contacto</HeaderLink></HeaderLi>
-            <HeaderLi><HeaderLink to="/Carrito" onClick={handleNav}>Carrito</HeaderLink></HeaderLi>
-          </HeaderUl>
-        </HeaderNav>
+          <BoxContainer>
+            <HeaderNav open={open}>
+              <HeaderUl>
+                <HeaderLi><HeaderLink onClick={handleNav} to="/">Inicio</HeaderLink></HeaderLi>
+                <HeaderLi><HeaderLink onClick={handleNav} to="/servicios">Servicios</HeaderLink></HeaderLi>
+                <HeaderLi><HeaderLink onClick={handleNav} to="/Tienda">Tienda</HeaderLink></HeaderLi>
+                <HeaderLi><HeaderLink onClick={handleNav} to="/Contacto">Contacto</HeaderLink></HeaderLi>
+              </HeaderUl>
+            </HeaderNav>
 
-        <CartContainer to="/carrito">
-          <CartIcon src={CARRITO_ICONO} alt="Icono de carrito de compras" />
-        </CartContainer>
+            {!userData ? (
+              <>
+                <HeaderButton $type="A" to="/login">Iniciar Sesión</HeaderButton>
+                <HeaderButton $type="B" to="/sign-up">Registrarse</HeaderButton>
+              </>
+            ) :
+              <>
+                <HeaderButton $type="A">{userData.user_name}</HeaderButton>
+                <HeaderButton $type="B" onClick={() => { handleSession() }}>Cerrar Sesión</HeaderButton>
+              </>
+            }
 
+            <Span />
+
+            <CartContainer to="/carrito">
+              <CartIcon src={CARRITO_ICONO} alt="Icono de carrito de compras" />
+              <BoxCounter>
+                <ProductCounter>{state.cart.length}</ProductCounter>
+              </BoxCounter>
+            </CartContainer>
+
+          </BoxContainer>
+
+        </HeaderBox>
       </HeaderContainer>
 
       <Outlet />
--- Frontend/src/components/general/header/HeaderStyled.jsx
+++ Frontend/src/components/general/header/HeaderStyled.jsx
@@ -3,50 +3,74 @@ import { Link } from 'react-router-dom';
 import { colores } from '../../../assets/css/Colors';
 
 export const HeaderContainer = styled.header`
-  background-color: ${props => props.$backgroundColor};
+  background-color: ${props => props.$scrolling ? colores.colorPrincipal : props.$backgroundColor};
   width: 100%;
   height: 60px;
+  padding: 0 60px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  padding: 0 10%;
-  position: absolute;
+  position: fixed;
   z-index: 10;
-
-  @media screen and (max-width: 1024px){
-    padding: 0 8%;
-  }
+  transition: background-color 0.5s ease;
 
   @media screen and (max-width: 768px){
-    flex-wrap: wrap;
-    padding: 0;
+    padding: 0 20px;
   }
-`;
+`
+
+export const HeaderBox = styled.div`
+  height: 100%;
+  width: 100%;
+  max-width: 1400px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
 export const LogoContainer = styled(Link)`
   display: flex;
-`;
+
+  @media screen and (max-width: 850px){
+    display: none;
+  }
+`
 
 export const LogoImage = styled.img`
   width: 30px;
+`
 
-  @media screen and (max-width: 768px){
-    display: none;
-  }
-`;
+export const BoxContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`
 
 export const HeaderNav = styled.nav`
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-  @media screen and (max-width: 768px){
+  @media screen and (max-width: 1110px){
+    position: static;
+    transform: none;
+  }
+
+  @media screen and (max-width: 850px){
     background-color: ${colores.colorPrincipal};
+    position: absolute;
     width: 100vw;
+    left: 0px;
+    right: 0px;
+    top: 0px;
     height: 0;
     opacity: 0;
-    margin-top: -60px;
     overflow: hidden;
     transition: all 0.5s ease;
     ${props => props.open && css`
@@ -54,7 +78,7 @@ export const HeaderNav = styled.nav`
       opacity: 1;
     `}
   }
-`;
+`
 
 export const HeaderUl = styled.ul`
   display: flex;
@@ -63,30 +87,93 @@ export const HeaderUl = styled.ul`
   gap: 1.875rem;
   list-style: none;
 
-  @media screen and (max-width: 768px){
+  @media screen and (max-width: 850px){
     flex-direction: column;
     gap: 3.75rem;
   }
-`;
+`
 
 export const HeaderLi = styled.li`
-  font-family: NewYork;
-  font-size: 17.6px;
-  letter-spacing: 0.25rem;
+  font-family: Nunito;
+  font-size: 1.1rem;
 `;
 
 export const HeaderLink = styled(Link)`
   color: ${colores.colorBlanco};
 `;
 
+const buttonTypeA = css`
+  background-color: white;
+  color: ${colores.colorAcento};
+
+  &:hover{
+    background-color: ${colores.colorAcento};
+    color: white;
+  }
+`
+
+const buttonTypeB = css`
+  background-color: transparent;
+  color: white;
+  border: solid 1px white;
+
+  &:hover{
+    background-color: ${colores.colorAcento};
+    color: white;
+    border: solid 1px transparent;
+  }
+`
+
+export const HeaderButton = styled(Link)`
+  ${({ $type }) => ($type === 'B' ? buttonTypeB : buttonTypeA)}
+  font-family: "Nunito";
+  font-size: .9rem;
+  padding: 5px 15px;
+  border-radius: 100px;
+
+  @media screen and (max-width: 850px){
+    display: none;
+  }
+`
+
+export const Span = styled.span`
+  background-color: white;
+  height: 30px;
+  width: 2px;
+
+  @media screen and (max-width: 850px){
+    display: none;
+  }
+`
+
 export const CartContainer = styled(Link)`
   display: flex;
-`;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  z-index: 20;
+`
 
 export const CartIcon = styled.img`
   width: 30px;
+`
 
-  @media screen and (max-width: 768px){
-    display: none;
-  }
-`;
+export const BoxCounter = styled.div`
+  background-color: ${colores.colorPrincipal};
+  width: 16px;
+  height: 16px;
+  border-radius: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  right: -3px;
+  bottom: -3px;
+`
+
+export const ProductCounter = styled.span`
+  display: flex;
+  color: white;
+  font-size: .9rem;
+  margin-top: 2px;
+`
