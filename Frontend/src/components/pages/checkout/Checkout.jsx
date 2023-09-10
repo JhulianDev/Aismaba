76031f239cf2374180104de70e0f89d234672465
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-10 07:02:36 -0300

    Se crea la pagina checkout y sus estilos

---
+++ Frontend/src/components/pages/checkout/Checkout.jsx
@@ -0,0 +1,12 @@
+import TarjetaCheckout from "../../general/tarjetas/checkOut/TarjetaCheckOut";
+import { Section } from "./CheckoutStyled";
+
+const Checkout = () => {
+  return (
+    <Section>
+      <TarjetaCheckout />
+    </Section>
+  );
+};
+
+export default Checkout;
---
+++ Frontend/src/components/pages/checkout/CheckoutStyled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+import { colores } from "../../../assets/css/Colors"
+
+export const Section = styled.section`
+  background-color: ${colores.colorFondo};
+  width: 100%;
+  min-height: 100vh;
+  padding: 120px 60px 60px;
+  display: flex;
+  align-items: top;
+  justify-content: center;
+`
