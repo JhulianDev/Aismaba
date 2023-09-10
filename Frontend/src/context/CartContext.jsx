b08e8ae3185da43a028bf126527f9fc04c286088
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-10 06:58:54 -0300

    Se agregan nuevos estados al contexto

--- Frontend/src/context/CartContext.jsx
+++ Frontend/src/context/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useState } from "react";
 import CartReducer from "./CartReducer";
 
 const CartContext = createContext();
@@ -10,10 +10,12 @@ const CartProvider = ({ children }) => {
   }
 
   const [state, dispatch] = useReducer(CartReducer, initialState)
+  const [requireLogin, setRequireLogin] = useState(false)
+  const [order, setOrder] = useState()
 
 
   return (
-    <CartContext.Provider value={{ state, dispatch }}>
+    <CartContext.Provider value={{ state, dispatch, requireLogin, setRequireLogin, order, setOrder }}>
       {children}
     </CartContext.Provider>
   )
