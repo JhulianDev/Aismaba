3c9a08c33866bf36d73212baadc68b2935f3087e
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-10 07:03:54 -0300

    Se crea helper para el formateo de precios y calculo de valores totales

---
+++ Frontend/src/helpers/prices.js
@@ -0,0 +1,16 @@
+export const formatPrice = new Intl.NumberFormat("es-AR", {
+  currency: "ARS",
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+});
+
+export const calculateTotalValue = (items, currency, dolarValue) => {
+  let totalValue = 0;
+  items.forEach((item) => {
+    totalValue += currency === "ARS" ? item.precio * dolarValue : item.precio;
+  });
+
+  const totalValueFormatted = formatPrice.format(totalValue);
+
+  return totalValueFormatted;
+};
