dc56bc6b3ce94ba70bfe147a5b1d068651fc7bf2
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-10 07:03:18 -0300

    Se crea helper para el manejo de la creacion de ordenes de compra

---
+++ Frontend/src/helpers/order.js
@@ -0,0 +1,48 @@
+import axios from "axios"
+import Swal from "sweetalert2"
+import { getToken } from "./token";
+import { API_URL } from "../env/env";
+import { formatPrice } from "./prices";
+
+export const handleOrder = (state, userData, selectedCurrency, dolarValue, totalValue, setRequireLogin, setOrder, navigate) => {
+
+  if(!userData) {
+    Swal.fire({
+      icon: "info",
+      title: "Atención",
+      text: "Para continuar con la compra debe iniciar sesión",
+      showConfirmButton: true
+    })
+    setRequireLogin(true)
+    navigate("/login")
+    return;
+  }
+
+  const products = state.cart.map((item) => ({
+    tipo: item.tipo,
+    nombre: item.nombre,
+    precio: selectedCurrency === "ARS"
+      ? formatPrice.format(item.precio * dolarValue)
+      : formatPrice.format(item.precio),
+  }));
+
+  const dataOrder = {
+    user_id: userData.id,
+    products: products,
+    currency: selectedCurrency,
+    total: totalValue
+  }
+
+  axios.post(`${API_URL}/purchase-orders`, dataOrder, {
+    headers: {
+      Authorization: `Bearer ${getToken()}`
+    }
+  })
+    .then((resp) => {
+      setOrder(resp.data.order)
+      navigate("/checkout")
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
