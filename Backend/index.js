c32a114353d6d6f83a79f8ab865840823e5e136a
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-17 17:01:22 -0300

    Se agrega variable de entorno HOST y se modifica ruta raiz

--- Backend/index.js
+++ Backend/index.js
@@ -3,14 +3,14 @@ import cors from "cors";
 import router from './routes/router.js';
 import { dataBaseConnection } from './database/database.js';
 const PORT = process.env.PORT || 3000
-const HOST = '0.0.0.0';
+const HOST = process.env.HOST || '0.0.0.0';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", router)
+app.use("/api/v1", router)
 
 dataBaseConnection();
 
