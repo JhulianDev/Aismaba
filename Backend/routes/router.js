8beb6b644041f1033f66924cb3aa5d153042da82
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-17 17:01:42 -0300

    Se modifican los Path

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -8,30 +8,30 @@ import { register, login, getUser, purchaseOrder, updateOrder, getPurchases, cre
 import { validateAuthorization } from '../middlewares/authMiddleware.js';
 
 // Main path
-router.get("/api/v1", (req, res) => {
+router.get("/", (req, res) => {
   res.json("Welcome to Aismaba! This API is up and running.")
 })
 
 // Register and Login
-router.post("/api/v1/sign-up", register);
-router.post("/api/v1/login", login);
+router.post("/sign-up", register);
+router.post("/login", login);
 
 // Get User
-router.get("/api/v1/user", validateAuthorization, getUser);
+router.get("/user", validateAuthorization, getUser);
 
 // Purchase orders
-router.post("/api/v1/purchase-orders", validateAuthorization, purchaseOrder);
+router.post("/purchase-orders", validateAuthorization, purchaseOrder);
 
 // Update order
-router.put("/api/v1/order/:id", validateAuthorization, updateOrder);
+router.put("/order/:id", validateAuthorization, updateOrder);
 
 // Get Purchases
-router.get("/api/v1/purchases-user", validateAuthorization, getPurchases);
+router.get("/purchases-user", validateAuthorization, getPurchases);
 
 // Create Preference Mercado Pago
-router.post("/api/v1/mercado_pago/create_preference", validateAuthorization, createPreference);
+router.post("/mercado_pago/create_preference", validateAuthorization, createPreference);
 
 // Verify Payment Mercado Pago
-router.post("/api/v1/mercado_pago/verify_payment", validateAuthorization, verifyPaymentMP);
+router.post("/mercado_pago/verify_payment", validateAuthorization, verifyPaymentMP);
 
 export default router;
