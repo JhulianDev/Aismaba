38d2cc0ffc3b8787d40520c55a621566deeb3df4
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-17 04:52:14 -0300

    Se agrega variable de entorno

--- Frontend/src/env/env.js
+++ Frontend/src/env/env.js
@@ -1,3 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const TOKEN_NAME = import.meta.env.VITE_TOKEN_NAME
 export const PAYPAL_ID = import.meta.env.VITE_PAYPAL_ID
+export const MERCADOPAGO_KEY = import.meta.env.VITE_KEY_MERCADOPAGO
