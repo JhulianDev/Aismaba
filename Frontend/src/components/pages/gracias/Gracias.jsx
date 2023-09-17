4351070c30a9453ac89e34c5f5b4489ab97f9d6a
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-17 04:51:06 -0300

    Se agrega useEffect para manejar las compras de MercadoPago

--- Frontend/src/components/pages/gracias/Gracias.jsx
+++ Frontend/src/components/pages/gracias/Gracias.jsx
@@ -1,12 +1,37 @@
+import { useEffect } from "react";
+import axios from "axios";
 import { colores } from "../../../assets/css/Colors";
 import { LOTTIE_GRACIAS } from "../../../assets/lotties/lotties";
 import ButtonLink from "../../general/Buttons/contacto/ButtonLink";
-import ContactoButton from "../../general/Buttons/contacto/ButtonLink";
 import Footer from "../../general/footer/Footer";
-import { GraciasBox, GraciasBoxLottie, GraciasDetalles, GraciasParrafo, GraciasSection, GraciasSubtitulo, GraciasTitulo } from "./GraciasStyled";
+import { GraciasBox, GraciasBoxLottie, GraciasDetalles, GraciasSection, GraciasSubtitulo, GraciasTitulo } from "./GraciasStyled";
 import Lottie from "lottie-react"
+import { API_URL } from "../../../env/env";
+import { getToken } from "../../../helpers/token";
 
 const Gracias = () => {
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const payment_id = urlParams.get('payment_id');
+
+    // Verifica si hay payment_id antes de hacer la solicitud
+    if (payment_id) {
+      axios.post(`${API_URL}/mercado_pago/verify_payment`, { payment_id }, {
+        headers: {
+          Authorization: `Bearer ${getToken()}`
+        }
+      })
+        .then((response) => {
+          console.log('Respuesta de la petición:', response);
+        })
+        .catch((error) => {
+          console.error('Error en la petición:', error);
+        });
+    }
+  }, []);
+
+
   return (
     <>
       <GraciasSection>
