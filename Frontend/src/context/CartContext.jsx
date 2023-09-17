6bf4ae557be7a91cfc381af8bd1b86c9bfa9b71d
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-17 04:51:43 -0300

    Se agrega estado preferenceId

--- Frontend/src/context/CartContext.jsx
+++ Frontend/src/context/CartContext.jsx
@@ -12,10 +12,11 @@ const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(CartReducer, initialState)
   const [requireLogin, setRequireLogin] = useState(false)
   const [order, setOrder] = useState()
+  const [preferenceId, setPreferenceId] = useState(null);
 
 
   return (
-    <CartContext.Provider value={{ state, dispatch, requireLogin, setRequireLogin, order, setOrder }}>
+    <CartContext.Provider value={{ state, dispatch, requireLogin, setRequireLogin, order, setOrder, preferenceId, setPreferenceId }}>
       {children}
     </CartContext.Provider>
   )
