1362d5dc4fa2d307060b7d053702e015e413d935
JhulianCode <aguilera.jhulian@gmail.com>
2023-09-20 02:40:22 -0300

    Se agrega useEffect para obtener el dolar value

--- Frontend/src/context/CurrencyContext.jsx
+++ Frontend/src/context/CurrencyContext.jsx
@@ -1,14 +1,29 @@
-import { createContext, useState } from "react";
+import axios from "axios"
+import { createContext, useEffect, useState } from "react";
+import { API_URL } from "../env/env";
 
 const CurrencyContext = createContext();
 
 const CurrencyProvider = ({ children }) => {
   const [selectedCurrency, setSelectedCurrency] = useState("ARS");
-  const dolarValue = 350
+  const [dolarValue, setDolarValue] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await axios.get(`${API_URL}/private`);
+        setDolarValue(resp.data.dolar_value);
+      } catch (error) {
+        console.error(`Error al obtener el valor del dólar: ${error.response.data.message}`);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <CurrencyContext.Provider value={{ selectedCurrency, setSelectedCurrency, dolarValue }}>
-      { children }
+      {children}
     </CurrencyContext.Provider>
   )
 }
