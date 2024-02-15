ade8c4caf4d33ac326b46409774cc4f08e7935b8
JhulianDev <jhulian.dev@gmail.com>
2024-02-15 03:41:27 -0300

    Se importa archivo de fuentes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router'
 import "./assets/css/normalize.css"
+import './assets/fonts/Fonts.css'
 import { ShopProvider } from './context/ShopContext'
 import { CartProvider } from './context/CartContext'
 import { CurrencyProvider } from './context/CurrencyContext'
