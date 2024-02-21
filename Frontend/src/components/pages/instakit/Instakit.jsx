99b36ef6a62f075c5c31c02ac6564ff818de42f4
JhulianDev <jhulian.dev@gmail.com>
2024-02-21 03:47:01 -0300

    Se crea la pagina Instakit y se agrega la seccion Portada

---
+++ Frontend/src/components/pages/instakit/Instakit.jsx
@@ -0,0 +1,11 @@
+import PortadaServicio from "../../general/servicios/portada/PortadaServicio";
+
+const Instakit = () => {
+  return (
+    <>
+      <PortadaServicio />
+    </>
+  );
+};
+
+export default Instakit;
