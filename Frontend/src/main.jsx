88f2f729fe67d9410d3b3fb52ce09834e047248a
JhulianDev <jhulian.dev@gmail.com>
2024-02-25 04:27:33 -0300

    Se importa el CurrencyProviderV2

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,15 +7,18 @@ import { ShopProvider } from './context/ShopContext'
 import { CartProvider } from './context/CartContext'
 import { CurrencyProvider } from './context/CurrencyContext'
 import { UserProvider } from './context/UserContext'
+import { CurrencyProviderV2 } from './context/CurrencyContextV2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserProvider>
-    <CurrencyProvider>
-      <CartProvider>
-        <ShopProvider>
-          <RouterProvider router={Router} />
-        </ShopProvider>
-      </CartProvider>
-    </CurrencyProvider>
+    <CurrencyProviderV2>
+      <CurrencyProvider>
+        <CartProvider>
+          <ShopProvider>
+            <RouterProvider router={Router} />
+          </ShopProvider>
+        </CartProvider>
+      </CurrencyProvider>
+    </CurrencyProviderV2>
   </UserProvider>
 )
