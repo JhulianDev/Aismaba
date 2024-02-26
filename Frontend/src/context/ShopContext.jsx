a56cf4547351cf81b14ce394aa2634b7c7fc6f43
JhulianDev <jhulian.dev@gmail.com>
2024-02-26 06:07:52 -0300

    Se agregan faqs a servicio Instakit

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 import { AESTHETIC_BROWN_1, CATEGORIA_MARCAS, CATEGORIA_PLANTILLAS, CATEGORIA_PRESETS, CURVE_PINK_1, ELEGANT_INSPO_1, GREEN_WORLD_1, MINIMAL_MOOD_1, MIX_NUDE_1, OCEAN_BLUE_1, PORTADA_ALMA_STUDIO, ROSE_LOVE_1, SOL_GREY_1, aestheticBrownImages, almaStudioContent, almaStudioImages, curvePinkImages, elegantInspoImages, greenWorldImages, minimalMoodImages, mixNudeImages, oceanBlueImages, roseLoveImages, solGreyImages } from "../assets/img/images";
 import { PRODUCT_AESTHETIC_BROWN, PRODUCT_CURVE_PINK, PRODUCT_ELEGANT_INSPO, PRODUCT_GREEN_WORLD, PRODUCT_MINIMAL_MOOD, PRODUCT_MIX_NUDE, PRODUCT_OCEAN_BLUE, PRODUCT_ROSE_LOVE, PRODUCT_SOL_GREY } from "../assets/products/products";
+import { v4 as uuidv4 } from 'uuid';
 
 const shopData = {
   categorias: [
@@ -44,7 +45,7 @@ const shopData = {
     { nombre : "Mix Nude",  descargable: PRODUCT_MIX_NUDE }
   ],
   instakit: {
-    precio: { ARS: "99.999", USD: "99.99", EUR: "92.99" },
+    precio: { ARS: "89.999", USD: "99.99", EUR: "92.99" },
     items: [
       "6 PLANTILLAS POST SIMPLES",
       "3 PLANTILLAS PORTADAS REELS",
@@ -55,6 +56,28 @@ const shopData = {
       "DISEÑO DE FEED",
       "PALETA DE COLORES",
       "TIPOGRAFIAS"
+    ],
+    faqs: [
+      {
+        id: uuidv4(),
+        pregunta: "Pueden vosotros encargaros de las fotos y textos de mi web?",
+        respuesta: "Si has empezado con los textos de tu web y se te han atragantado. No te preocupes, para eso estamos nosotros."
+      },
+      {
+        id: uuidv4(),
+        pregunta: "Ya tengo una web, pero quiero cambiarla. ¿Puedes ayudarme?",
+        respuesta: "¡Claro que sí! Haremos un rediseño integral de la web, para que tu negocio pueda seguir creciendo."
+      },
+      {
+        id: uuidv4(),
+        pregunta: "Me crearás las cuentas de correo y de redes sociales?",
+        respuesta: "Sí, daremos de alta una cuenta de correo electrónico corporativo con el nombre de tu web."
+      },
+      {
+        id: uuidv4(),
+        pregunta: "Por qué no me entra el dominio en el paquete web?",
+        respuesta: "El nombre de la empresa es tuyo, por eso el dominio es algo que debe ser de tu propiedad."
+      }
     ]
   }
 }
