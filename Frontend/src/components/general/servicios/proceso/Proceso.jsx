84101f406288face87850286a9c8a840e64e9f6c
JhulianDev <jhulian.dev@gmail.com>
2024-03-06 02:32:34 -0300

    Se reemplaza el contenido por props

--- Frontend/src/components/general/servicios/proceso/Proceso.jsx
+++ Frontend/src/components/general/servicios/proceso/Proceso.jsx
@@ -1,76 +1,27 @@
 import { coloresV2 } from "../../../../assets/css/Colors";
 import { MaxWidth, Section } from "../../../../assets/styles/GeneralStyles";
 import { Box, Description, Image, MainContainer, Step, Subtitle, Title } from "./ProcesoStyles";
-import IMAGE_PROCESS from "../../../../assets/img/generales/portada-tienda.png"
 
-const Proceso = () => {
+const Proceso = ({ title, steps, image }) => {
   return (
     <Section $height="100px" $borderBottom $bgColor={coloresV2.colorPrincipal}>
       <MaxWidth>
         <MainContainer>
 
           <Box $title>
-            <Title>Proceso de desarrollo</Title>
+            <Title>{title}</Title>
           </Box>
 
-          <Box>
-            <Step>01</Step>
-            <Subtitle>LLAMADA INICIAL</Subtitle>
-            <Description>
-              Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!
-            </Description>
-          </Box>
-
-          <Box>
-            <Step>02</Step>
-            <Subtitle>LLAMADA INICIAL</Subtitle>
-            <Description>
-              Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!
-            </Description>
-          </Box>
-
-          <Box>
-            <Step>03</Step>
-            <Subtitle>LLAMADA INICIAL</Subtitle>
-            <Description>
-              Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!
-            </Description>
-          </Box>
-
-          <Box>
-            <Step>04</Step>
-            <Subtitle>LLAMADA INICIAL</Subtitle>
-            <Description>
-              Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!
-            </Description>
-          </Box>
-
-          <Box>
-            <Step>05</Step>
-            <Subtitle>LLAMADA INICIAL</Subtitle>
-            <Description>
-              Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!
-            </Description>
-          </Box>
-
-          <Box>
-            <Step>06</Step>
-            <Subtitle>LLAMADA INICIAL</Subtitle>
-            <Description>
-              Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!
-            </Description>
-          </Box>
-
-          <Box>
-            <Step>07</Step>
-            <Subtitle>LLAMADA INICIAL</Subtitle>
-            <Description>
-              Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!
-            </Description>
-          </Box>
+          {steps.map((step) => (
+            <Box key={step.id}>
+              <Step>{step.number}</Step>
+              <Subtitle>{step.subtitle}</Subtitle>
+              <Description>{step.description}</Description>
+            </Box>
+          ))}
 
           <Box $image>
-            <Image src={IMAGE_PROCESS} alt="Mockup Servicio" />
+            <Image src={image} alt="Mockup Servicio" />
           </Box>
 
         </MainContainer>
