3aa4c81561dde097b92e0c0ad0fd44f20b48dd66
JhulianDev <jhulian.dev@gmail.com>
2024-03-07 05:05:43 -0300

    Se agrega informacion sobre la seccion Preguntas frecuentes

--- Frontend/src/content/CreacionContenidoData.jsx
+++ Frontend/src/content/CreacionContenidoData.jsx
@@ -20,9 +20,9 @@ export const data = {
     image: PORTADA_INSTAKIT
   },
   benefits: [
-    { id: uuidv4(), icon: ICONO_DOMINIO, title: "Dominio Gratis", description: "De regalo por un año" },
-    { id: uuidv4(), icon: ICONO_HOSTING, title: "Hosting Gratis", description: "De regalo por un año" },
-    { id: uuidv4(), icon: ICONO_RESPONSIVE, title: "Diseño Adaptable", description: "A todos los dispositivos" }
+    { id: uuidv4(), icon: ICONO_DOMINIO, title: "Diseño de Feed", description: "Estetico y funcional" },
+    { id: uuidv4(), icon: ICONO_HOSTING, title: "Edición de Reels", description: "De regalo por un año" },
+    { id: uuidv4(), icon: ICONO_RESPONSIVE, title: "Ideas de Contenido", description: "A todos los dispositivos" }
   ],
   pillars: {
     presentation: "En AISMABA, ofrecemos un servicio completo para mejorar tu presencia en Instagram.",
@@ -58,43 +58,43 @@ export const data = {
         id: uuidv4(),
         number: "01",
         subtitle: "LLAMADA INICIAL",
-        description: "Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!"
+        description: "Agendamos una videollamada para compartir todos los detalles de tus ideas y ponerte al tanto sobre cada detalle de nuestro trabajo el primer mes y los meses siguientes."
       },
       {
         id: uuidv4(),
         number: "02",
-        subtitle: "LLAMADA INICIAL",
-        description: "Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!"
+        subtitle: "BRIEF",
+        description: "Te enviaré un formulario donde obtendremos la máxima información de tu marca. Conoceremos a fondo tu proyecto."
       },
       {
         id: uuidv4(),
         number: "03",
-        subtitle: "LLAMADA INICIAL",
-        description: "Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!"
+        subtitle: "INVESTIGACIÓN",
+        description: "Pautaremos una reunión vía chat de WhatsApp, para aclarar dudas del Brief, y concretar como llevaremos a cabo la estrategia e imagen."
       },
       {
         id: uuidv4(),
         number: "04",
-        subtitle: "LLAMADA INICIAL",
-        description: "Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!"
+        subtitle: "ESTRATEGIA",
+        description: "Realizaremos un diagnostico de tu perfil y desarrollaremos una estrategia digital específica para la esencia de tu marca."
       },
       {
         id: uuidv4(),
         number: "05",
-        subtitle: "LLAMADA INICIAL",
-        description: "Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!"
+        subtitle: "CALENDARIO MENSUAL",
+        description: "Elaboraremos mes a mes un calendario de ideas de contenido basados en los objetivos del mes."
       },
       {
         id: uuidv4(),
         number: "06",
-        subtitle: "LLAMADA INICIAL",
-        description: "Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!"
+        subtitle: "DISEÑO Y EDICIÓN",
+        description: "Prepararemos el contenido, estructura de posts, guion y edición para los videos, redacción de copies. Diseño de las piezas de contenido."
       },
       {
         id: uuidv4(),
         number: "07",
-        subtitle: "LLAMADA INICIAL",
-        description: "Nos reuniremos en una videollamada para conocernos y hablar sobre las necesidades que tiene tu marca. ¡Queremos conocer tu historia para transmitir tu esencia y valores únicos!"
+        subtitle: "PUBLICACIÓN",
+        description: "Una vez aprobado y culminado cada uno de las piezas de contenido del Feed, programare mediante una aplicación para que se suban automáticamente el contenido del Feed a Instagram."
       }
     ]
   },
@@ -167,6 +167,42 @@ export const data = {
         { id: uuidv4(), description: "Diseño del Feed." }
       ]
     },
+    link: "https://wa.link/xc6bbv"
+  },
+  faqs: [
+    {
+      id: uuidv4(),
+      question: "¿Cómo haríamos el contenido si somos de distintos países?",
+      answer: "Podemos trabajar desde cualquier parte del mundo, todo de forma online. Nosotros Llevaremos a cabo la estructura de cómo serán los Reels, el guion, te daremos técnicas para que tu hagas la grabación, y luego editamos los videos."
+    },
+    {
+      id: uuidv4(),
+      question: "¿Ustedes me darán las ideas de contenido para mis publicaciones?",
+      answer: "¡Claro que sí! Llevaremos a cabo las ideas mes a mes para desarrollar todo el contenido de la marca. Elaboraremos desde las ideas, hasta plasmarlas en los posts y videos, y luego redactar los copies para que esté listo para publicar."
+    },
+    {
+      id: uuidv4(),
+      question: "Si ya tengo los colores y tipografías, pero quiero cambiar ¿Podrías ayudarme?",
+      answer: "¡Por supuesto! Te ayudaremos a definir una imagen completa, que incluye los colores, las tipografías, y el estilo de fotografías."
+    },
+    {
+      id: uuidv4(),
+      question: "Ya tengo una imagen que quiero conservar. ¿Se puede adaptar al servicio que estás ofreciendo?",
+      answer: "Claro. Podemos respetar los lineamientos de tu imagen existente, y lograr que todo quede perfectamente armónico sin perder su esencia original."
+    },
+    {
+      id: uuidv4(),
+      question: "Si aún no tengo un perfil ¿Me crearan todo desde cero?",
+      answer: "Te asesoramos como crear el perfil nuevo, con tu correo, nombre de usuario. Y te darémos las herramientas necesarias para que coloques en cada parte del perfil. Eso acompañado de la imagen que prepararemos como el diseño del Feed y las portadas de historias destacadas."
+    },
+    {
+      id: uuidv4(),
+      question: "¿Ustedes manejarán la cuenta? ¿Crearán el contenido y lo publicarán?",
+      answer: "Programaremos mediante una aplicación la carga automática del contenido exclusivo del Feed a Instagram. Pero el seguimiento del perfil, las publicaciones de las historias, los mensajes y comentarios corren por tu cuenta."
+    }
+  ],
+  contact: {
+    cta: "¿Tienes dudas sobre como comenzar? Contáctanos y te asesoraremos.",
     link: "https://wa.link/xc6bbv"
   }
 }
