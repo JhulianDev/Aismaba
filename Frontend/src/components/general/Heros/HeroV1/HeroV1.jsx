f806f263153a69157f153bcf8db3714a8ae65def
JhulianDev <jhulian.dev@gmail.com>
2024-03-10 23:11:37 -0300

    Se crea la seccion HeroV1 y sus estilos

---
+++ Frontend/src/components/general/Heros/HeroV1/HeroV1.jsx
@@ -0,0 +1,35 @@
+import { MaxWidth, Section } from "../../../../assets/styles/GeneralStyles";
+import { BoxImage, BoxTag, BoxTexts, Button, ContainerHero, Description, Icon, Image, Tag, Subtitle } from "./HeroV1Styles";
+
+const HeroV1 = ({ tag, icon, title, description, callToAction, link, image, alt }) => {
+  return (
+    <Section>
+      <MaxWidth>
+        <ContainerHero>
+
+          <BoxTag $mobile>
+            <Tag>{tag}</Tag>
+          </BoxTag>
+
+          <BoxTexts>
+            <BoxTag>
+              <Icon src={icon} />
+              <Tag>{tag}</Tag>
+            </BoxTag>
+
+            <Subtitle>{title}</Subtitle>
+            <Description>{description}</Description>
+            <Button to={link}>{callToAction}</Button>
+          </BoxTexts>
+
+          <BoxImage>
+            <Image src={image} alt={alt} />
+          </BoxImage>
+
+        </ContainerHero>
+      </MaxWidth>
+    </Section>
+  );
+};
+
+export default HeroV1;
