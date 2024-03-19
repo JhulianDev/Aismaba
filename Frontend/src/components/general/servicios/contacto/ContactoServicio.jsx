f13fa0e3649cb9b086167c28132b8e72d1effb73
JhulianDev <jhulian.dev@gmail.com>
2024-03-19 03:49:17 -0300

    Se agrega prop para manejar el color de fondo del componente

--- Frontend/src/components/general/servicios/contacto/ContactoServicio.jsx
+++ Frontend/src/components/general/servicios/contacto/ContactoServicio.jsx
@@ -1,9 +1,9 @@
 import { MaxWidth, Section } from "../../../../assets/styles/GeneralStyles";
 import { Button, Parrafo } from "./ContactoServicioStyles";
 
-const ContactoServicio = ({ cta, link }) => {
+const ContactoServicio = ({ cta, link, bgColor }) => {
   return (
-    <Section $height="100px">
+    <Section $height="100px" $bgColor={bgColor}>
       <MaxWidth $flexDirection="column">
         <Parrafo>{cta}</Parrafo>
         <Button to={link} target="_blank">Contactar</Button>
