2e02325ee254ff426e299b78f08ae597f0720f97
JhulianDev <jhulian.dev@gmail.com>
2024-03-19 05:37:44 -0300

    Se agrega informacion de la seccion Preguntas frecuentes

--- Frontend/src/content/WebDesignData.jsx
+++ Frontend/src/content/WebDesignData.jsx
@@ -149,5 +149,69 @@ export const data = {
       }
     ]
   },
-  portfolio: [ PORTFOLIO_01, PORTFOLIO_02, PORTFOLIO_03, PORTFOLIO_04 ]
+  portfolio: [
+    PORTFOLIO_01,
+    PORTFOLIO_02,
+    PORTFOLIO_03,
+    PORTFOLIO_04
+  ],
+  faqs: [
+    {
+      id: uuidv4(),
+      question: "¿Ustedes se encargarán de las fotos y textos de mi web?",
+      answer: "¡Por supuesto! Nos encargaremos de crear los textos y seleccionar las imágenes adecuadas para tu sitio web. Buscaremos transmitir tu mensaje de manera efectiva y atractiva ¡hasta conseguir que tu web se vea increíble!"
+    },
+    {
+      id: uuidv4(),
+      question: "¿Qué harán en el servicio de mantenimiento web?",
+      answer:
+        <>
+          <b>· Actualizaremos</b> tus productos, tus servicios y sus precios para que la información de tu web este siempre al día.
+          <br />
+          <br />
+          <b>· Revisaremos mensualmente tu sitio web</b> con el objetivo de preveer posibles fallos y aplicar soluciones inmediatas.
+          <br />
+          <br />
+          <b>· Realizaremos las actualizaciones</b> necesarias para cumplir con los últimos Estándares de seguridad y garantizar la integridad de tu web, protegiendo así los datos de tus usuarios.
+          <br />
+          <br />
+          <b>· Optimizaremos la velocidad de tu web</b>, para ofrecerle a tus usuarios una experiencia de uso gratificante.
+          <br />
+          <br />
+          <b>· Actualizaremos</b> los textos e imágenes de las secciones existentes cuando lo necesites.
+          <br />
+          <br />
+          <b>· Crearemos</b> hasta 3 nuevas secciones al año (no páginas).
+        </>
+    },
+    {
+      id: uuidv4(),
+      question: "¿Cuáles son los métodos de pago disponibles?",
+      answer:
+        <>
+          · <b>Argentina:</b> podrás realizar el pago en pesos a través de MercadoPago o transferencia bancaria.
+          <br />
+          <br />
+          · <b>Otros países:</b> te enviaremos un enlace de PayPal, con el que podrás pagar usando cualquier tarjeta de débito o crédito de tu país.
+          <br />
+          <br />
+          · <b>Otros métodos de pago:</b> Binance, Payonner.
+        </>
+    },
+    {
+      id: uuidv4(),
+      question: "¿Qué costo tienen el hosting y el dominio después del primer año gratis?",
+      answer:
+        <>
+          · Tras disfrutar de este servicio durante <b>el primer año de forma gratuita</b>, el mantenimiento del <b>Hosting</b> y <b>Dominio</b> de tu web tendrá un costo de <b>120 dólares</b> que solo deberás abonar <b>una vez al año.</b>
+          <br />
+          <br />
+          · Esta inversión <b>garantiza</b> que tu página siga <b>en línea y accesible para tu audiencia</b>, ademas de permitirte <b>conservar el nombre</b> de tu pagina web (Dominio).
+        </>
+    }
+  ],
+  contact: {
+    cta: "¿Tienes dudas sobre como comenzar? Contáctanos y te asesoraremos.",
+    link: "https://wa.link/njjz31"
+  }
 }
