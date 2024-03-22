02d16c468fd20bce06c12ce7fd6012f42fa83e92
JhulianDev <jhulian.dev@gmail.com>
2024-03-22 07:05:16 -0300

    Se actualiza informacion de la Data

--- Frontend/src/content/CreacionContenidoData.jsx
+++ Frontend/src/content/CreacionContenidoData.jsx
@@ -1,21 +1,40 @@
 import { v4 as uuidv4 } from 'uuid';
-import PORTADA_INSTAKIT from "../assets/img/generales/portada_instakit.png"
-import ICONO_DOMINIO from "../assets/img/generales/Icono-Dominio.svg"
-import ICONO_HOSTING from "../assets/img/generales/Icono-Hosting.svg"
-import ICONO_RESPONSIVE from "../assets/img/generales/Icono-Responsive.svg"
+// Section Hero
+import LIGHT_ICON from "../assets/img/generales/light_icon.svg";
+import HERO_CREACION_CONTENIDO from "../assets/img/generales/contenido/hero_creacion_contenido.webp";
+// Section Benefits
+import GUION_ICON from "../assets/img/generales/contenido/guion_icon_creacion_contenido.svg"
+import FEED_ICON from "../assets/img/generales/contenido/feed_icon_creacion_contenido.svg"
+import DIAMOND_ICON from "../assets/img/generales/contenido/diamond_icon_creacion_contenido.svg"
+// Section Portfolio
+import PORTFOLIO_CONTENIDO_01 from "../assets/img/generales/contenido/portfolio_creacion_contenido_01.webp"
+import PORTFOLIO_CONTENIDO_02 from "../assets/img/generales/contenido/portfolio_creacion_contenido_02.webp"
+import PORTFOLIO_CONTENIDO_03 from "../assets/img/generales/contenido/portfolio_creacion_contenido_03.webp"
+import PORTFOLIO_CONTENIDO_04 from "../assets/img/generales/contenido/portfolio_creacion_contenido_04.webp"
+import PORTFOLIO_CONTENIDO_05 from "../assets/img/generales/contenido/portfolio_creacion_contenido_05.webp"
+import PORTFOLIO_CONTENIDO_06 from "../assets/img/generales/contenido/portfolio_creacion_contenido_06.webp"
+import PORTFOLIO_CONTENIDO_07 from "../assets/img/generales/contenido/portfolio_creacion_contenido_07.webp"
+import PORTFOLIO_CONTENIDO_08 from "../assets/img/generales/contenido/portfolio_creacion_contenido_08.webp"
+import PORTFOLIO_CONTENIDO_09 from "../assets/img/generales/contenido/portfolio_creacion_contenido_09.webp"
+import PORTFOLIO_CONTENIDO_10 from "../assets/img/generales/contenido/portfolio_creacion_contenido_10.webp"
+// Section Process
 import IMAGE_PROCESS from "../assets/img/generales/portada-tienda.webp"
 
 export const data = {
-  portada: {
-    title: "Contenido",
-    subtitle: "En Redes Sociales",
-    description: "Que impulsa tu marca al siguiente nivel ",
-    image: PORTADA_INSTAKIT
+  hero: {
+    tag: "CREACIÓN DE CONTENIDO",
+    icon: LIGHT_ICON,
+    title: "Creación de Contenido para emprendedoras.",
+    description: "Diseñamos la estrategia perfecta para impulsar tu negocio en las redes sociales.",
+    callToAction: "¡Lo Quiero!",
+    link: "https://wa.link/5x3oun",
+    image: HERO_CREACION_CONTENIDO,
+    alt: "MockUp Servicios"
   },
   benefits: [
-    { id: uuidv4(), icon: ICONO_DOMINIO, title: "Diseño de Feed", description: "Estetico y funcional" },
-    { id: uuidv4(), icon: ICONO_HOSTING, title: "Edición de Reels", description: "De regalo por un año" },
-    { id: uuidv4(), icon: ICONO_RESPONSIVE, title: "Ideas de Contenido", description: "A todos los dispositivos" }
+    { id: uuidv4(), icon: GUION_ICON, title: "Guiones", description: "Ideas para tus Reels" },
+    { id: uuidv4(), icon: FEED_ICON, title: "Diseño de feed", description: "Estético y Funcional" },
+    { id: uuidv4(), icon: DIAMOND_ICON, title: "Ideas de contenido", description: "Especificas para tu marca" }
   ],
   pillars: {
     presentation: "En AISMABA, ofrecemos un servicio completo para mejorar tu presencia en Instagram.",
@@ -127,7 +146,7 @@ export const data = {
         { id: uuidv4(), description: "Propuesta de Feed." }
       ]
     },
-    link: "https://wa.link/xc6bbv"
+    link: "https://wa.link/xeaycn"
   },
   packMesPorMes: {
     namePack: "PACK MES POR MES",
@@ -160,7 +179,23 @@ export const data = {
         { id: uuidv4(), description: "Diseño del Feed." }
       ]
     },
-    link: "https://wa.link/xc6bbv"
+    link: "https://wa.link/wr86qg"
+  },
+  portfolio: {
+    title: "Nuestro portafolio",
+    subtitle: "¡Parte de nuestra magia!",
+    images: [
+      PORTFOLIO_CONTENIDO_01,
+      PORTFOLIO_CONTENIDO_02,
+      PORTFOLIO_CONTENIDO_03,
+      PORTFOLIO_CONTENIDO_04,
+      PORTFOLIO_CONTENIDO_05,
+      PORTFOLIO_CONTENIDO_06,
+      PORTFOLIO_CONTENIDO_07,
+      PORTFOLIO_CONTENIDO_08,
+      PORTFOLIO_CONTENIDO_09,
+      PORTFOLIO_CONTENIDO_10
+    ]
   },
   faqs: [
     {
@@ -175,8 +210,8 @@ export const data = {
     },
     {
       id: uuidv4(),
-      question: "Si ya tengo los colores y tipografías, pero quiero cambiar ¿Podrías ayudarme?",
-      answer: "¡Por supuesto! Te ayudaremos a definir una imagen completa, que incluye los colores, las tipografías, y el estilo de fotografías."
+      question: "¿Ustedes manejarán la cuenta? ¿Crearán el contenido y lo publicarán?",
+      answer: "Programaremos mediante una aplicación la carga automática del contenido exclusivo del Feed a Instagram. Pero el seguimiento del perfil, las publicaciones de las historias, los mensajes y comentarios corren por tu cuenta."
     },
     {
       id: uuidv4(),
@@ -186,16 +221,25 @@ export const data = {
     {
       id: uuidv4(),
       question: "Si aún no tengo un perfil ¿Me crearan todo desde cero?",
-      answer: "Te asesoramos como crear el perfil nuevo, con tu correo, nombre de usuario. Y te darémos las herramientas necesarias para que coloques en cada parte del perfil. Eso acompañado de la imagen que prepararemos como el diseño del Feed y las portadas de historias destacadas."
+      answer: "Te asesoramos como crear el perfil nuevo, con tu correo, nombre de usuario. Y te darémos las herramientas necesarias para que optimices cada parte del perfil. Eso acompañado de la imagen que prepararemos como el diseño del Feed y las portadas de historias destacadas."
     },
     {
       id: uuidv4(),
-      question: "¿Ustedes manejarán la cuenta? ¿Crearán el contenido y lo publicarán?",
-      answer: "Programaremos mediante una aplicación la carga automática del contenido exclusivo del Feed a Instagram. Pero el seguimiento del perfil, las publicaciones de las historias, los mensajes y comentarios corren por tu cuenta."
-    }
+      question: "¿Cuáles son los métodos de pago disponibles?",
+      answer:
+        <>
+          · <b>Argentina:</b> podrás realizar el pago en pesos a través de MercadoPago o transferencia bancaria.
+          <br />
+          <br />
+          · <b>Otros países:</b> te enviaremos un enlace de PayPal, con el que podrás pagar usando cualquier tarjeta de débito o crédito de tu país.
+          <br />
+          <br />
+          · <b>Otros métodos de pago:</b> Binance, Payonner.
+        </>
+    },
   ],
   contact: {
     cta: "¿Tienes dudas sobre como comenzar? Contáctanos y te asesoraremos.",
-    link: "https://wa.link/xc6bbv"
+    link: "https://wa.link/5x3oun"
   }
 }
