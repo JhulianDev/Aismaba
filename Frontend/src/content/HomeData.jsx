0cf2ff2af382ec07bbeebba13493a1f0d7ba0992
JhulianDev <jhulian.dev@gmail.com>
2024-03-28 04:04:13 -0300

    Se crea el archivo HomeData

---
+++ Frontend/src/content/HomeData.jsx
@@ -0,0 +1,12 @@
+export const data = {
+  hero: {
+    tag: "AISMABA - AGENCIA CREATIVA",
+    title: "Diseños increibles para",
+    uppercase: "EMPRENDEDORAS",
+    description: "Impulsamos tu marca en las redes sociales.",
+    textButtonA: "Creación de contenido",
+    linkButtonA: "/creacion-de-contenido",
+    textButtonB: "Plantillas Instagram",
+    linkButtonB: "/Tienda",
+  }
+}
