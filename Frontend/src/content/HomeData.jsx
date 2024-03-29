16d1c472e04535b85ffad8565ffada91e5c3f270
JhulianDev <jhulian.dev@gmail.com>
2024-03-29 04:58:30 -0300

    Se agrega data de la Card Shop

--- Frontend/src/content/HomeData.jsx
+++ Frontend/src/content/HomeData.jsx
@@ -1,3 +1,9 @@
+// Section Hero
+import HERO_HOME from "../assets/img/generales/home/hero_home.webp"
+import HERO_HOME_MOBILE from "../assets/img/generales/home/hero_home_mobile.webp"
+// Section Hub
+import PLANTILLAS_HOME from "../assets/img/generales/home/plantillas_home.webp";
+
 export const data = {
   hero: {
     tag: "AISMABA - AGENCIA CREATIVA",
@@ -8,6 +14,18 @@ export const data = {
     linkButtonA: "/creacion-de-contenido",
     textButtonB: "Plantillas Instagram",
     linkButtonB: "/Tienda",
+    image: HERO_HOME,
+    imageMobile: HERO_HOME_MOBILE
   },
-  separator: "INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES · INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES · INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES · INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES"
+  separator: "INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES · INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES · INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES · INSTAKIT · CREACIÓN DE CONTENIDO · DISEÑO WEB · PLANTILLAS EDITABLES",
+  hub: {
+    cardShop: {
+      title: "Plantillas Editables",
+      description: "Dale una imagen profesional, atractiva y armoniosa a tu perfil de Instagram.",
+      cta: "Visitar Tienda",
+      link: "/tienda",
+      image: PLANTILLAS_HOME,
+      alt: "MockUp Plantillas"
+    }
+  }
 }
