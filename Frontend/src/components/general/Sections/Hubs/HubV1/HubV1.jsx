85206fbf6575920a194aa4c89fccb66679fabf00
JhulianDev <jhulian.dev@gmail.com>
2024-03-30 07:27:42 -0300

    Se agregan etiquetas Title y Subtitle y sus estilos

--- Frontend/src/components/general/Sections/Hubs/HubV1/HubV1.jsx
+++ Frontend/src/components/general/Sections/Hubs/HubV1/HubV1.jsx
@@ -2,12 +2,14 @@ import { coloresV2 } from "../../../../../assets/css/Colors";
 import { MaxWidth, Section } from "../../../../../assets/styles/GeneralStyles";
 import CardV1 from "../../../cards/CardV1/CardV1";
 import CardV2 from "../../../cards/CardV2/CardV2";
-import { BoxCards } from "./HubV1Styles";
+import { BoxCards, Subtitle, Title } from "./HubV1Styles";
 
 const HubV1 = ({ dataCardShop, dataCardsServices }) => {
   return (
-    <Section $height="100px" $bgColor={coloresV2.colorSecundario}>
+    <Section $height="100px" $bgColor={coloresV2.colorSecundario} $borderBottom>
       <MaxWidth $flexDirection="column">
+        <Title>CONOCE NUESTROS</Title>
+        <Subtitle>SERVICIOS</Subtitle>
         <CardV1 data={dataCardShop} />
 
         <BoxCards>
--- Frontend/src/components/general/Sections/Hubs/HubV1/HubV1Styles.jsx
+++ Frontend/src/components/general/Sections/Hubs/HubV1/HubV1Styles.jsx
@@ -1,4 +1,63 @@
 import styled from "styled-components";
+import { fonts } from "../../../../../assets/fonts/FontsHandler";
+import { coloresV2 } from "../../../../../assets/css/Colors";
+
+export const Title = styled.h1`
+  font-family: ${fonts.MainTypography};
+  color: ${coloresV2.colorTextos};
+  font-size: 2.5rem;
+  text-align: center;
+  font-weight: 100;
+  margin-bottom: 10px;
+  margin-top: -10px;
+
+  @media screen and (max-width: 1535px){
+    font-size: 2.2rem;
+    margin-bottom: 8px;
+  }
+
+  @media screen and (max-width: 1232px){
+    font-size: 2rem;
+    margin-bottom: 6px;
+  }
+
+  @media screen and (max-width: 1024px){
+    font-size: 1.9rem;
+    margin-bottom: 5px;
+  }
+
+  @media screen and (max-width: 768px){
+    font-size: 1.8rem;
+    margin: -25px 0;
+  }
+`
+
+export const Subtitle = styled.h2`
+  font-family: ${fonts.SecondaryTypography};
+  color: ${coloresV2.colorTextos};
+  text-align: center;
+  font-size: 1.4rem;
+  font-weight: 400;
+  margin-bottom: 30px;
+
+  @media screen and (max-width: 1535px){
+    font-size: 1.3rem;
+    margin-bottom: 25px;
+  }
+
+  @media screen and (max-width: 1232px){
+    font-size: 1.2rem;
+    margin-bottom: 20px;
+  }
+
+  @media screen and (max-width: 1024px){
+    font-size: 1.1rem;
+  }
+
+  @media screen and (max-width: 768px){
+    margin-bottom: -10px;
+  }
+`
 
 export const BoxCards = styled.div`
   width: 100%;
