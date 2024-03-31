b8610b9e517c9e6f0a61a18fe42359333d72be4b
JhulianDev <jhulian.dev@gmail.com>
2024-03-31 07:58:35 -0300

    Se agrega nueva informacion a Data

--- Frontend/src/content/HomeData.jsx
+++ Frontend/src/content/HomeData.jsx
@@ -7,6 +7,13 @@ import PLANTILLAS_HOME from "../assets/img/generales/home/plantillas_home.webp";
 import CREACION_CONTENIDO_HOME from "../assets/img/generales/home/creacion_de_contenido_home.webp"
 import INSTAKIT_HOME from "../assets/img/generales/home/instakit_home.webp"
 import DISENO_WEB_HOME from "../assets/img/generales/home/diseno_web_home.webp"
+// Section Testimonials
+import LOGO_MULATAS_CAKE from "../assets/img/generales/home/mulatas_cake_logo.svg"
+import LOGO_HI_PARTY from "../assets/img/generales/home/hi_party_logo.svg"
+import LOGO_HABIGAIL from "../assets/img/generales/home/habigail_logo.svg"
+import LOGO_LUMEN from "../assets/img/generales/home/lumen_logo.svg"
+// Section Contact
+import MOCKUP_CONTACT from "../assets/img/generales/home/mockup_contact.webp"
 
 export const data = {
   hero: {
@@ -32,9 +39,56 @@ export const data = {
       alt: "MockUp Plantillas"
     },
     cardsServices: [
-      { id: uuidv4(), title: "Creación de contenido", image: CREACION_CONTENIDO_HOME, alt: "MockUp Creación de contenido", link: "/creacion-de-contenido"  },
-      { id: uuidv4(), title: "Instakit", image: INSTAKIT_HOME, alt: "MockUp Instakit", link: "/instakit"  },
-      { id: uuidv4(), title: "Diseño web", image: DISENO_WEB_HOME, alt: "MockUp Diseño Web", link: "/diseno-web"  }
+      { id: uuidv4(), title: "Creación de contenido", image: CREACION_CONTENIDO_HOME, alt: "MockUp Creación de contenido", link: "/creacion-de-contenido" },
+      { id: uuidv4(), title: "Instakit", image: INSTAKIT_HOME, alt: "MockUp Instakit", link: "/instakit" },
+      { id: uuidv4(), title: "Diseño web", image: DISENO_WEB_HOME, alt: "MockUp Diseño Web", link: "/diseno-web" }
     ]
+  },
+  testimonials: [
+    {
+      id: uuidv4(),
+      name: "MULATAS CAKE",
+      category: "Pasteleria",
+      testimony: "Fue la mejor decisión el haber contratado sus servicios de Branding. Al principio estaba nerviosa porque no tenia una idea clara de que imagen.",
+      logo: LOGO_MULATAS_CAKE,
+      alt: "Logo Mulatas Cake"
+    },
+    {
+      id: uuidv4(),
+      name: "HI PARTY",
+      category: "Accesorios",
+      testimony: "Fue la mejor decisión el haber contratado sus servicios de Branding. Al principio estaba nerviosa porque no tenia una idea clara de que imagen.",
+      logo: LOGO_HI_PARTY,
+      alt: "Logo Hi Party"
+    },
+    {
+      id: uuidv4(),
+      name: "HABIGAIL",
+      category: "Indumentaria",
+      testimony: "Fue la mejor decisión el haber contratado sus servicios de Branding. Al principio estaba nerviosa porque no tenia una idea clara de que imagen.",
+      logo: LOGO_HABIGAIL,
+      alt: "Logo Habigail"
+    },
+    {
+      id: uuidv4(),
+      name: "LUMEN",
+      category: "Consultoria",
+      testimony: "Fue la mejor decisión el haber contratado sus servicios de Branding. Al principio estaba nerviosa porque no tenia una idea clara de que imagen.",
+      logo: LOGO_LUMEN,
+      alt: "Logo Lumen"
+    },
+  ],
+  contact: {
+    title:
+      <>
+        Las grandes ideas tienen
+        <br />
+        pequeños comienzos
+      </>,
+    subtitle: "HOY ES EL MEJOR DIA PARA COMENZAR",
+    image: MOCKUP_CONTACT,
+    alt: "MockUp Templates",
+    cta: "Contactanos",
+    link: "https://wa.link/cdi3nc"
   }
 }
