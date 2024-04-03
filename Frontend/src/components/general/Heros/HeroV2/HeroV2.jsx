c39511f67ea0d9b357f9a8d822f2c9d1083ee81b
JhulianDev <jhulian.dev@gmail.com>
2024-04-03 03:15:05 -0300

    Se corrige manejo de imagenes del Hero

--- Frontend/src/components/general/Heros/HeroV2/HeroV2.jsx
+++ Frontend/src/components/general/Heros/HeroV2/HeroV2.jsx
@@ -2,8 +2,6 @@ import { MaxWidth, Section } from "../../../../assets/styles/GeneralStyles";
 import { BoxButtons, BoxImage, BoxTexts, Button, ContainerHero, Description, Image, Title, Uppercase } from "./HeroV2Styles";
 import { BoxTag, Icon, Tag } from "../HeroV1/HeroV1Styles";
 import { coloresV2 } from "../../../../assets/css/Colors";
-import HERO_HOME from "../../../../assets/img/generales/home/hero_home.webp"
-import HERO_HOME_MOBILE from "../../../../assets/img/generales/home/hero_home_mobile.webp"
 import LIGHT_ICON from "../../../../assets/img/generales/light_icon.svg";
 
 const HeroV2 = ({ data, bgColor }) => {
@@ -33,8 +31,8 @@ const HeroV2 = ({ data, bgColor }) => {
           </BoxTexts>
 
           <BoxImage>
-            <Image src={HERO_HOME} alt="MockUp Aismaba" />
-            <Image src={HERO_HOME_MOBILE} alt="MockUp Aismaba" $mobile />
+            <Image src={data.image} alt="MockUp Aismaba" />
+            <Image src={data.imageMobile} alt="MockUp Aismaba" $mobile />
           </BoxImage>
 
         </ContainerHero>
