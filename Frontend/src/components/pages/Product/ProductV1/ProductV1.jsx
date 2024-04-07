4f98bd1166ac39e01ebbe943837da2c09e381d8c
JhulianDev <jhulian.dev@gmail.com>
2024-04-07 01:04:44 -0300

    Se crea la pagina ProductV1 y sus estilos

---
+++ Frontend/src/components/pages/Product/ProductV1/ProductV1.jsx
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import shopStore from "../../../../stores/shopStore"
+import { MaxWidth, Section } from "../../../../assets/styles/GeneralStyles";
+import SliderV2 from "../../../general/sliders/SliderV2/SliderV2";
+import { coloresV2 } from "../../../../assets/css/Colors";
+
+const ProductV1 = () => {
+  const { TemplatesData } = shopStore();
+  const { id } = useParams();
+  const template = TemplatesData.find(template => template.id === id);
+
+  return (
+    <Section $bgColor={coloresV2.colorSecundario}>
+      <MaxWidth $justifyContent="center">
+        <SliderV2 product={template} />
+      </MaxWidth>
+    </Section>
+  );
+};
+
+export default ProductV1;
