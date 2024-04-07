7f745467caa6a1f3b4e62dc9e166065553346234
JhulianDev <jhulian.dev@gmail.com>
2024-04-07 01:06:25 -0300

    Se agrega informacion de productos

--- Frontend/src/content/ShopData.jsx
+++ Frontend/src/content/ShopData.jsx
@@ -2,18 +2,10 @@ import { v4 as uuidv4 } from 'uuid';
 // Hero
 import SHOP_ICON from "../assets/img/generales/shop/hero/shop_icon.svg"
 import SOCIAL_ICONS from "../assets/img/generales/shop/hero/social_icons.svg"
-import COVER_HERO_01 from "../assets/img/generales/shop/hero/cover_hero_01.png"
-import COVER_HERO_02 from "../assets/img/generales/shop/hero/cover_hero_02.png"
-import COVER_HERO_03 from "../assets/img/generales/shop/hero/cover_hero_03.png"
-import COVER_HERO_04 from "../assets/img/generales/shop/hero/cover_hero_04.png"
-import COVER_HERO_05 from "../assets/img/generales/shop/hero/cover_hero_05.png"
-import COVER_HERO_06 from "../assets/img/generales/shop/hero/cover_hero_06.png"
-import COVER_HERO_07 from "../assets/img/generales/shop/hero/cover_hero_07.png"
-import COVER_HERO_08 from "../assets/img/generales/shop/hero/cover_hero_08.png"
-import COVER_HERO_09 from "../assets/img/generales/shop/hero/cover_hero_09.png"
-import COVER_HERO_10 from "../assets/img/generales/shop/hero/cover_hero_10.png"
-import COVER_HERO_11 from "../assets/img/generales/shop/hero/cover_hero_11.png"
-import COVER_HERO_12 from "../assets/img/generales/shop/hero/cover_hero_12.png"
+import { HERO_COVERS } from '../assets/img/generales/shop/hero/HERO_COVERS';
+// Templates
+import { AESTHETIC_BROWN_IMAGES } from "../assets/img/generales/shop/templates/01_AESTHETIC_BROWN/AESTHETIC_BROWN_IMAGES"
+import { NEUTRAL_MINIMALIST_IMAGES } from '../assets/img/generales/shop/templates/02_NEUTRAL_MINIMALIST/NEUTRAL_MINIMALIST_IMAGES';
 
 export const data = {
   hero: {
@@ -22,22 +14,13 @@ export const data = {
     iconTexts: SOCIAL_ICONS,
     title: "Plantillas Editables",
     description: "¡Dale una imagen atractiva a tu marca!",
-    covers: [
-      COVER_HERO_01,
-      COVER_HERO_02,
-      COVER_HERO_03,
-      COVER_HERO_04,
-      COVER_HERO_05,
-      COVER_HERO_06,
-      COVER_HERO_07,
-      COVER_HERO_08,
-      COVER_HERO_09,
-      COVER_HERO_10,
-      COVER_HERO_11,
-      COVER_HERO_12
-    ]
+    covers: HERO_COVERS
   },
+}
+
+export const products = {
   templates: [
-    { id: uuidv4(), name: "Minimal Mood", price: { ARS: "99.999", USD: "99.99", EUR: "92.99" }, coverImage: "", galleryImages: "" }
+    { id: "1", name: "Aesthetic Brown", description: "Plantillas Instagram", price: { ARS: "6.000", USD: "30", EUR: "28" }, coverImage: (AESTHETIC_BROWN_IMAGES.coverImage), galleryImages: (AESTHETIC_BROWN_IMAGES.galleryImages) },
+    { id: "2", name: "Neutral Minimalist", description: "Plantillas Instagram", price: { ARS: "6.000", USD: "30", EUR: "28" }, coverImage: (NEUTRAL_MINIMALIST_IMAGES.coverImage), galleryImages: (NEUTRAL_MINIMALIST_IMAGES.galleryImages) }
   ]
 }
