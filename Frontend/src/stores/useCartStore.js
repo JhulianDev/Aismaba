2526dd709d4dcc19fc8a0efd41c51043be5c4098
JhulianDev <jhulian.dev@gmail.com>
2024-04-10 11:57:07 -0300

    Se crear funciones para el manejor del estado del carrito

--- Frontend/src/stores/useCartStore.js
+++ Frontend/src/stores/useCartStore.js
@@ -1,16 +1,36 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware';
 
-const useCartStore = create((set, get) => ({
-  cartItems: [],
-  addToCart: (productId) => {
-    const state = get();
-    const isProductInCart = state.cartItems.some((item) => item.id === productId);
-    if (isProductInCart) {
-      return;
-    } else {
-      set({ cartItems: [...state.cartItems, productId] });
+const useCartStore = create(
+  persist(
+    (set, get) => ({
+      cartItems: [],
+      totalPrice: (currencySelected) => {
+        const state = get();
+        const total = state.cartItems.reduce((acc, item) => {
+          return acc + parseFloat(item.price[currencySelected]);
+        }, 0);
+        return total;
+      },
+      addToCart: (product) => {
+        const state = get();
+        const isProductInCart = state.cartItems.some((item) => item.id === product.id);
+        if (!isProductInCart) {
+          set({ cartItems: [...state.cartItems, product] });
+        }
+      },
+      removeFromCart: (productId) => {
+        const state = get();
+        set({ cartItems: state.cartItems.filter((item) => item.id !== productId) });
+      },
+      clearCart: () => {
+        set({ cartItems: [] });
+      }
+    }),
+    {
+      name: 'cart-storage'
     }
-  }
-}))
+  )
+);
 
 export default useCartStore;
