5503e0037e023c5d0802246031b755c6a85fe24a
JhulianDev <jhulian.dev@gmail.com>
2024-04-10 11:56:40 -0300

    Se eliminan providers sin uso

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,22 +3,13 @@ import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router'
 import "./assets/css/normalize.css"
 import './assets/fonts/Fonts.css'
-import { ShopProvider } from './context/ShopContext'
 import { CartProvider } from './context/CartContext'
-import { CurrencyProvider } from './context/CurrencyContext'
 import { UserProvider } from './context/UserContext'
-import { CurrencyProviderV2 } from './context/CurrencyContextV2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserProvider>
-    <CurrencyProviderV2>
-      <CurrencyProvider>
-        <CartProvider>
-          <ShopProvider>
-            <RouterProvider router={Router} />
-          </ShopProvider>
-        </CartProvider>
-      </CurrencyProvider>
-    </CurrencyProviderV2>
+    <CartProvider>
+      <RouterProvider router={Router} />
+    </CartProvider>
   </UserProvider>
 )
