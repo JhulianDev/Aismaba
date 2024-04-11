d6cbd293470c289a9498cbd7cf02c68e0055ce16
JhulianDev <jhulian.dev@gmail.com>
2024-04-11 16:28:20 -0300

    Se reestructura el componente y su funcionalidad

--- Frontend/src/components/pages/login/Login.jsx
+++ Frontend/src/components/pages/login/Login.jsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { MaxWidth, Section } from "../../../assets/styles/GeneralStyles";
 import { coloresV2 } from "../../../assets/css/Colors";
+import Loader from '../../general/Loader/Loader';
 import LoginCard from "../../general/tarjetas/login/LoginCard";
+import { handleSignIn } from "../../../helpers/handleSignIn";
+import useUserStore from "../../../stores/useUserStore";
+import { setToken } from "../../../helpers/token";
+import { API_URL } from "../../../env/env";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const { setUserData, requireLogin } = useUserStore();
+  const navigate = useNavigate();
+  const redirectLink = "/";
+  const apiUrl = (`${API_URL}/login`);
+
   return (
     <Section $bgColor={coloresV2.colorSecundario}>
       <MaxWidth $justifyContent="center">
 
-        <LoginCard
-          type="Login"
-          submitText="Iniciar Sesión"
-          redirectText="Registrarse"
-          linkButton="/sign-up"
-          apiUrl="/login"
-          redirectLink="/"
-        />
+        {loading ? (
+          <Loader height={"100vh"} />
+        ) : (
+          <LoginCard
+            type="Login"
+            link="/sign-up"
+            handleSubmit={(e) => handleSignIn(e, setLoading, apiUrl, setUserData, setToken, navigate, requireLogin, redirectLink)}
+          />
+        )}
 
       </MaxWidth>
     </Section>
--- Frontend/src/components/pages/signUp/SignUp.jsx
+++ Frontend/src/components/pages/signUp/SignUp.jsx
@@ -1,20 +1,31 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { MaxWidth, Section } from "../../../assets/styles/GeneralStyles";
 import { coloresV2 } from "../../../assets/css/Colors";
 import LoginCard from "../../general/tarjetas/login/LoginCard";
+import { handleSignUp } from "../../../helpers/handleSignUp";
+import { API_URL } from "../../../env/env";
+import Loader from '../../general/Loader/Loader';
 
 const SignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const redirectLink = "/login";
+  const apiUrl = (`${API_URL}/sign-up`);
+
   return (
     <Section $bgColor={coloresV2.colorSecundario}>
       <MaxWidth $justifyContent="center">
 
-        <LoginCard
-          type="SignUp"
-          submitText="Registrarse"
-          redirectText="Iniciar Sesión"
-          linkButton="/login"
-          apiUrl="/sign-up"
-          redirectLink="/login"
-        />
+        {loading ? (
+          <Loader height={"100vh"} />
+        ) : (
+          <LoginCard
+            type="SignUp"
+            link="/login"
+            handleSubmit={(e) => handleSignUp(e, setLoading, apiUrl, navigate, redirectLink)}
+          />
+        )}
 
       </MaxWidth>
     </Section>
@@ -22,3 +33,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
