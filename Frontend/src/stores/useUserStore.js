dcbe13220229345319472777905634592f84a28e
JhulianDev <jhulian.dev@gmail.com>
2024-04-11 18:35:41 -0300

    Se crea metodo loadUserData

--- Frontend/src/stores/useUserStore.js
+++ Frontend/src/stores/useUserStore.js
@@ -5,18 +5,52 @@ const useUserStore = create(
   persist(
     (set, get) => ({
       userData: {},
-      requiresLogin: false,
       setUserData: (data) => {
         set({ userData: data })
       },
+
+      userToken: "",
+      setUserToken: (token) => {
+        set({ userToken: token })
+      },
+
+      loadUserData: () => {
+        const userToken = get().userToken;
+        if (userToken) {
+          console.log(userToken)
+        } else {
+          console.log("No user token")
+        }
+      },
+      //   const userToken = get().userToken;
+      //   if (userToken) {
+      //     axios.get(`${API_URL}/user`, {
+      //       headers: {
+      //         Authorization: `Bearer ${userToken}`
+      //       }
+      //     })
+      //       .then((resp) => {
+      //         setUserData(resp.data.user)
+      //       })
+      //       .catch((error) => {
+      //         console.error(`Error al obtener el usuario: ${error.response.data.message}`);
+      //       });
+      //   }
+      // },
+
+      requiresLogin: false,
       setRequiresLogin: (isLoginRequired) => {
         set({ requiresLogin: isLoginRequired })
-      }
+      },
+
+      setConsole: (console.log("Hola"))
     }),
     {
       name: 'user-storage'
     }
   )
 );
+
+useUserStore.getState().loadUserData();
 
 export default useUserStore;
