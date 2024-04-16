50473643c840fa1e56f798c180f458c3936446ab
JhulianDev <jhulian.dev@gmail.com>
2024-04-16 16:09:20 -0300

    Se implementa uso de data

--- Frontend/src/components/general/Sections/Forms/FormV1/FormV1.jsx
+++ Frontend/src/components/general/Sections/Forms/FormV1/FormV1.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 import { MaxWidth, Section } from '../../../../../assets/styles/GeneralStyles';
 import { BoxImage, BoxInputs, Button, CardContainer, Description, Form, Image, Input, Title } from './FormV1Styles';
-import IMAGE_FORM from "../../../../../assets/img/generales/plantillas_gratuitas/image_form.jpg"
 import { coloresV2 } from '../../../../../assets/css/Colors';
 import { handleSuscription } from '../../../../../helpers/handleNewsletter';
 import Loader from '../../../Loader/Loader';
 
-const FormV1 = () => {
+const FormV1 = ({ data, formRef }) => {
   const [formData, setFormData] = useState({ user_name: "", email: "", country: "" });
   const [loading, setLoading] = useState(false);
   return (
-    <Section $bgColor={coloresV2.colorSecundario}>
+    <Section $bgColor={coloresV2.colorSecundario} ref={formRef}>
       <MaxWidth>
         <CardContainer>
           <BoxImage>
-            <Image src={IMAGE_FORM} alt='MockUp Plantillas' />
+            <Image src={data.image} alt={data.alt} />
           </BoxImage>
 
           <Form onSubmit={(e) => handleSuscription(e, setLoading, setFormData)} noValidate>
@@ -25,14 +24,14 @@ const FormV1 = () => {
               />
             ) : (
               <>
-                <Title>Descarga las plantillas</Title>
-                <Description>Completa el formulario y recibe tus plantillas gratis!</Description>
+                <Title>{data.title}</Title>
+                <Description>{data.description}</Description>
                 <BoxInputs>
                   <Input type="text" name="user_name" placeholder="Nombre" value={formData.user_name} onChange={(e) => setFormData({ ...formData, user_name: e.target.value })} />
                   <Input type="text" name="country" placeholder="País" value={formData.country} onChange={(e) => setFormData({ ...formData, country: e.target.value })} />
                 </BoxInputs>
                 <Input type="email" name="email" placeholder="Email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
-                <Button type='submit'>Descargar plantillas</Button>
+                <Button type='submit'>{data.callToAction}</Button>
               </>
             )}
           </Form>
