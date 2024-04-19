2ac45e1e287916a40cf14b3310ba38d157b821a0
JhulianDev <jhulian.dev@gmail.com>
2024-04-19 01:49:35 -0300

    Se configura manejo de paypal

--- Frontend/src/components/general/paypal/PaypalPayment.jsx
+++ Frontend/src/components/general/paypal/PaypalPayment.jsx
@@ -1,14 +1,10 @@
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-import { API_URL, PAYPAL_ID } from "../../../env/env";
-import { CartContext } from "../../../context/CartContext";
-import { getToken } from "../../../helpers/token";
-import { useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
 import Swal from "sweetalert2"
-import axios from "axios"
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { PAYPAL_ID } from "../../../env/env";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const PaypalPayment = ({ value, orderId, setLoading }) => {
-  const { dispatch, setOrder } = useContext(CartContext)
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,52 +15,28 @@ const PaypalPayment = ({ value, orderId, setLoading }) => {
     <PayPalScriptProvider options={{ clientId: PAYPAL_ID }}>
       <PayPalButtons
         createOrder={(_, actions) => {
-          // Paso 1: Crear una orden en PayPal
+          // Creamos una orden en PayPal con el monto total y el id de la orden
           return actions.order.create({
             purchase_units: [
               {
                 amount: {
-                  value: value, // El valor total de la orden
+                  value: value,
                 },
-                custom_id: orderId // Identificador personalizado de la orden
+                custom_id: orderId
               },
             ],
           })
         }}
+        // Función que se ejecuta cuando se aprueba el pago
         onApprove={(_, actions) => {
-          // Paso 2: Capturar el pago cuando se aprueba
+          // Capturamos el pago cuando se aprueba
           return actions.order.capture().then((resp) => {
+            // Si el pago se completa con éxito
             if (resp.status === "COMPLETED") {
-              // Paso 3: Si el pago se completa con éxito
-
-              // Paso 3.1: Actualizar la orden en tu servidor
-              axios.put(`${API_URL}/order/${orderId}`, null, {
-                headers: {
-                  Authorization: `Bearer ${getToken()}` // Autenticación con token
-                }
-              })
-                .then((resp) => {
-                  // Paso 3.2: Limpia la orden y el carrito después del pago exitoso
-                  setOrder(undefined);
-                  dispatch({ type: "CLEAR_CART" });
-
-                  // Paso 3.3: Muestra un mensaje de éxito al usuario
-                  Swal.fire({
-                    icon: "success",
-                    title: "Éxito",
-                    text: `¡Pago Exitoso!`,
-                    showConfirmButton: false,
-                    timer: 1800 // Tiempo para mostrar el mensaje
-                  })
-
-                  // Paso 3.4: Redirige al usuario a la página de agradecimiento
-                  navigate("/gracias-compra");
-                })
-                .catch((error) => {
-                  console.log(error); // Manejo de errores en caso de fallo en la actualización
-                })
+              // Redirigimos al usuario a la página de compra realizada
+              navigate(`/compra-realizada/?order_id=${orderId}`);
             } else {
-              // Paso 4: Si el pago no se procesa con éxito, muestra un mensaje de atención al usuario
+              // Si el pago no se procesa con éxito, muestramos un mensaje de error al usuario
               Swal.fire({
                 icon: "info",
                 title: "Atención",
