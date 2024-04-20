9fc9e0e3738dd0b5c291dc7f3429e4058bc3fec1
JhulianDev <jhulian.dev@gmail.com>
2024-04-20 05:44:11 -0300

    Se modifica logica de la pagina MisCompras y sus estilos

--- Frontend/src/components/pages/misCompras/MisCompras.jsx
+++ Frontend/src/components/pages/misCompras/MisCompras.jsx
@@ -1,72 +1,51 @@
 import axios from "axios"
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { DOWNLOAD_ICON } from "../../../assets/img/images";
-import { BoxCompras, BoxItem, ColumnDownload, ColumnProduct, DownloadBox, DownloadIcon, Product, Section, Title } from "./MisComprasStyled";
-import { getToken } from "../../../helpers/token";
+import { CardContainer, BoxItem, Subtitle, DownloadButton, DownloadIcon, Product, Title, Paragraph } from "./MisComprasStyled";
 import { API_URL } from "../../../env/env";
-import { ShopContext } from "../../../context/ShopContext";
 import Loader from "../../general/Loader/Loader";
+import { MaxWidth, Section } from "../../../assets/styles/GeneralStyles";
+import { coloresV2 } from "../../../assets/css/Colors";
+import useUserStore from "../../../stores/useUserStore";
+import { productsFiles } from "../../../assets/products/productsFiles";
 
 const MisCompras = () => {
-  const [productos, setproductos] = useState()
-  const [descargables, setDescargables] = useState([]);
-  const { products } = useContext(ShopContext)
+  const [purchasedProducts, setPurchasedProducts] = useState(null);
   const [loading, setLoading] = useState(true);
+  const { userToken } = useUserStore.getState();
 
 
   useEffect(() => {
-    if (getToken()) {
-      axios.get(`${API_URL}/purchases-user`, {
-        headers: {
-          Authorization: `Bearer ${getToken()}`
-        }
-      })
-        .then((resp) => {
-          if (resp.data.purchases) {
-            // Constante para almacenar todas las compras
-            const purchases = resp.data.purchases;
-            // Constante para almacenar todos los productos de todas las compras
-            const allProducts = [];
-            // Iterar a través de las compras y extraer los productos
-            purchases.forEach((purchase) => {
-              // Removemos caracteres al principio y al final de la cadena
-              const jsonString = purchase.products.slice(2, -2);
-              // Reemplazamos las comillas escapadas para obtener un JSON válido
-              const cleanedString = jsonString.replace(/\\"/g, '"');
-
-              try {
-                // Analizamos la cadena como JSON
-                const productJson = JSON.parse(cleanedString);
-                allProducts.push(productJson);
-              } catch (error) {
-                console.error('Error al analizar JSON:', error);
-              }
-            });
-
-            // Guardamos los productos en el estado "compras"
-            setproductos(allProducts)
-
-            // Almacenamos los descargables correspondientes a cada producto en una constante:
-            const descargablesDeProductos = allProducts.map((producto) => {
-              // Buscamos el producto correspondiente en el estado "products" usando el nombre del producto.
-              const productoEncontrado = products.find((p) => p.nombre === producto.nombre);
-              // Si se encuentra el producto en "products", tomamos su propiedad "descargable", de lo contrario, establecemos una cadena vacía.
-              return productoEncontrado ? productoEncontrado.descargable : "";
-            });
-
-            // Establecemos el estado "descargables" con la lista de descargables correspondientes a los productos.
-            setDescargables(descargablesDeProductos);
+    // Funcion para obtener las ordenes completadas por el usuario
+    const fetchData = async () => {
+      try {
+        // Realizamos la solicitud a la ruta indicada para procesar el pago
+        const response = await axios.get(`${API_URL}/orders-completed-user`, {
+          headers: {
+            Authorization: `Bearer ${userToken}`
           }
-
-        })
-        .catch((error) => {
-          console.log(`Error al obtener las compras: ${error}`);
-        })
-        .finally(() => {
-          setLoading(false);
         });
-    }
-  }, [])
+        // Si la petición es exitosa:
+        // Mostramos la respuesta del backend
+        console.log('Respuesta de la petición:', response);
+        // Filtramos los productos comprados por el usuario segun los productsId recibidos
+        const filteredProducts = productsFiles.filter(product => response.data.productsId.includes(product.id));
+        // Seteamos los productos 
+        setPurchasedProducts(filteredProducts);
+        // Detenemos el loading
+        setLoading(false);
+      } catch (error) {
+        // Si la petición da error:
+        // Mostramos el error de la petición
+        console.error('Error en la petición:', error);
+        // Detenemos el loading
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
 
   if (loading)
     return (
@@ -77,36 +56,43 @@ const MisCompras = () => {
     )
 
   return (
-    <Section>
-      <BoxCompras>
+    <Section $bgColor={coloresV2.colorSecundario} $align="flex-start">
+      <MaxWidth>
 
-        <BoxItem>
-          <Title>Mis compras</Title>
-        </BoxItem>
-
-        {productos && productos.length > 0 ? (
-          <>
+        {loading ? (
+          <Loader
+            height={"100px"}
+          />
+        ) : (
+          <CardContainer>
             <BoxItem>
-              <ColumnProduct>Productos</ColumnProduct>
-              <ColumnDownload>Descargar</ColumnDownload>
+              <Title>Mis compras</Title>
             </BoxItem>
 
-            {productos.map((producto, index) => (
-              <BoxItem key={index}>
-                <Product>{producto.nombre}</Product>
-                <DownloadBox href={descargables[index]} download>
-                  <DownloadIcon src={DOWNLOAD_ICON} />
-                </DownloadBox>
-              </BoxItem>
-            ))}
-          </>
-        ) : (
-          <BoxItem>
-            <ColumnProduct>Aqui podras descargar los productos que adquieras en nuestra tienda.</ColumnProduct>
-          </BoxItem>
-        )}
+            {purchasedProducts.length > 0 ? (
+              <>
+                <BoxItem>
+                  <Subtitle>Productos</Subtitle>
+                  <Subtitle $download>Descargar</Subtitle>
+                </BoxItem>
 
-      </BoxCompras>
+                {purchasedProducts.map((product) => (
+                  <BoxItem key={product.id}>
+                    <Product>{product.name}</Product>
+                    <DownloadButton href={product.file} download>
+                      <DownloadIcon src={DOWNLOAD_ICON} />
+                    </DownloadButton>
+                  </BoxItem>
+                ))}
+              </>
+            ) : (
+              <BoxItem>
+                <Paragraph>Aqui podras descargar los productos que adquieras en nuestra tienda.</Paragraph>
+              </BoxItem>
+            )}
+          </CardContainer>
+        )}
+      </MaxWidth>
     </Section>
   );
 };
--- Frontend/src/components/pages/misCompras/MisComprasStyled.jsx
+++ Frontend/src/components/pages/misCompras/MisComprasStyled.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
-import { colores } from "../../../assets/css/Colors"
+import { colores, coloresV2 } from "../../../assets/css/Colors"
+import { fonts } from "../../../assets/fonts/FontsHandler"
 
 export const Section = styled.section`
   background-color: ${colores.colorFondo};
@@ -17,13 +18,15 @@ export const Section = styled.section`
   }
 `
 
-export const BoxCompras = styled.div`
-  background-color: #ffffff;
-  height: fit-content;
-  max-width: 1200px;
+export const CardContainer = styled.div`
+  background-color: ${coloresV2.colorPrincipal};
+  height: fit -content;
   width: 100%;
   padding: 15px 40px 45px;
   border-radius: 8px;
+  margin-top: 60px;
+  border: 1px solid ${coloresV2.colorTextos};
+  font-family: ${fonts.SecondaryTypography};
   overflow: hidden;
 
   box-shadow:
@@ -37,10 +40,25 @@ export const BoxCompras = styled.div`
 
 export const Title = styled.h1`
   width: 100%;
+  font-family: ${fonts.MainTypography};
+  font-size: 1.8rem;
+  font-weight: bold;
   text-align: center;
-  font-family: Nunito;
-  font-size: 30px;
-  font-weight: 700;
+`
+
+export const Subtitle = styled.h2`
+  font-size: 1.4rem;
+  font-weight: bold;
+
+  @media screen and (max-width: 375px){
+    display: ${(props) => props.$download && "none"};
+  }
+`
+
+export const Paragraph = styled.p` 
+  width: 100%;
+  font-size: 1.4rem;
+  text-align: center;
 `
 
 export const BoxItem = styled.div`
@@ -53,28 +71,11 @@ export const BoxItem = styled.div`
 `
 
 export const Product = styled.h1`
-  font-family: Nunito;
-  font-size: 22px;
-  font-weight: 600;
+  font-size: 1.4rem;
+  font-weight: 100;
 `
 
-export const ColumnProduct = styled.h1`
-  font-family: Nunito;
-  font-size: 23px;
-  font-weight: 700;
-`
-
-export const ColumnDownload = styled.h1`
-  font-family: Nunito;
-  font-size: 23px;
-  font-weight: 700;
-
-  @media screen and (max-width: 375px){
-    display: none;
-  }
-`
-
-export const DownloadBox = styled.a`
+export const DownloadButton = styled.a`
   display: flex;
   margin-right: 30px;
   cursor: pointer;
@@ -87,5 +88,5 @@ export const DownloadBox = styled.a`
 
 export const DownloadIcon = styled.img`
   width: 50px;
-  filter: invert(51%) sepia(17%) saturate(1123%) hue-rotate(341deg) brightness(88%) contrast(79%);
+  filter: invert(100%);
 `
