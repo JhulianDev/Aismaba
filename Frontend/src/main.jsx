0ba9b74574c7d71822c3ba1e09e623edaee59064
JhulianDev <jhulian.dev@gmail.com>
2024-04-20 05:40:59 -0300

    Se eliminan providers

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,7 @@ import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router'
 import "./assets/css/normalize.css"
 import './assets/fonts/Fonts.css'
-import { CartProvider } from './context/CartContext'
-import { UserProvider } from './context/UserContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <UserProvider>
-    <CartProvider>
-      <RouterProvider router={Router} />
-    </CartProvider>
-  </UserProvider>
+  <RouterProvider router={Router} />
 )
