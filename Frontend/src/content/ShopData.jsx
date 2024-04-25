89437405e82f48cdfdf08738ae2ce8787d1f289e
JhulianDev <jhulian.dev@gmail.com>
2024-04-25 20:15:40 -0300

    Se agrega informacion de contenido de las plantillas

--- Frontend/src/content/ShopData.jsx
+++ Frontend/src/content/ShopData.jsx
@@ -33,24 +33,198 @@ export const data = {
   },
 }
 
+const contentTemplates = [
+  "9 Plantillas para Posts en formato vertical (1080 x 1350px)",
+  "6 Plantillas para Portadad de Reels en formato vertical (1080 x 1920px)",
+  "9 Plantillas para Historias en formato vertical (1080 x 1920px)",
+  "3 Plantillas para Carrusel de 5 Páginas c/u (1080 x 1350px)",
+  "6 Portadas para Historias Destacadas (1080 x 1920px)"
+]
+
 export const products = {
   templates: [
-    { id: "1", name: "Aesthetic Brown", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (AESTHETIC_BROWN_IMAGES.coverImage), galleryImages: (AESTHETIC_BROWN_IMAGES.galleryImages) },
-    { id: "2", name: "Neutral Minimalist", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (NEUTRAL_MINIMALIST_IMAGES.coverImage), galleryImages: (NEUTRAL_MINIMALIST_IMAGES.galleryImages) },
-    { id: "3", name: "Black Aesthetic", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (BLACK_AESTHETIC_IMAGES.coverImage), galleryImages: (BLACK_AESTHETIC_IMAGES.galleryImages) },
-    { id: "4", name: "Minimal Mood", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (MINIMAL_MOOD_IMAGES.coverImage), galleryImages: (MINIMAL_MOOD_IMAGES.galleryImages) },
-    { id: "5", name: "Rose Love", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (ROSE_LOVE_IMAGES.coverImage), galleryImages: (ROSE_LOVE_IMAGES.galleryImages) },
-    { id: "6", name: "Elegant Inspo", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (ELEGANT_INSPO_IMAGES.coverImage), galleryImages: (ELEGANT_INSPO_IMAGES.galleryImages) },
-    { id: "7", name: "Green Experiences", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (GREEN_EXPERIENCES_IMAGES.coverImage), galleryImages: (GREEN_EXPERIENCES_IMAGES.galleryImages) },
-    { id: "8", name: "Ocean Blue", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (OCEAN_BLUE_IMAGES.coverImage), galleryImages: (OCEAN_BLUE_IMAGES.galleryImages) },
-    { id: "9", name: "Mix Nude", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (MIX_NUDE_IMAGES.coverImage), galleryImages: (MIX_NUDE_IMAGES.galleryImages) },
-    { id: "10", name: "Sol Grey", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (SOL_GREY_IMAGES.coverImage), galleryImages: (SOL_GREY_IMAGES.galleryImages) },
-    { id: "11", name: "Curve Pink", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (CURVE_PINK_IMAGES.coverImage), galleryImages: (CURVE_PINK_IMAGES.galleryImages) },
-    { id: "12", name: "Green World", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (GREEN_WORLD_IMAGES.coverImage), galleryImages: (GREEN_WORLD_IMAGES.galleryImages) },
-    { id: "13", name: "Three Pack 01", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (THREE_PACK_01_IMAGES.coverImage), galleryImages: (THREE_PACK_01_IMAGES.galleryImages) },
-    { id: "14", name: "Three Pack 02", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (THREE_PACK_02_IMAGES.coverImage), galleryImages: (THREE_PACK_02_IMAGES.galleryImages) },
-    { id: "15", name: "Three Pack 03", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (THREE_PACK_03_IMAGES.coverImage), galleryImages: (THREE_PACK_03_IMAGES.galleryImages) },
-    { id: "16", name: "Three Pack 04", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (THREE_PACK_04_IMAGES.coverImage), galleryImages: (THREE_PACK_04_IMAGES.galleryImages) },
-    { id: "17", name: "Twelve Pack", type: "Plantillas Instagram", price: { ARS: "6000", USD: "30", EUR: "28" }, coverImage: (TWELVE_PACK_IMAGES.coverImage), galleryImages: (TWELVE_PACK_IMAGES.galleryImages) }
+    {
+      id: "1",
+      name: "Aesthetic Brown",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (AESTHETIC_BROWN_IMAGES.coverImage),
+      galleryImages: (AESTHETIC_BROWN_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "2",
+      name: "Neutral Minimalist",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (NEUTRAL_MINIMALIST_IMAGES.coverImage),
+      galleryImages: (NEUTRAL_MINIMALIST_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "3",
+      name: "Black Aesthetic",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (BLACK_AESTHETIC_IMAGES.coverImage),
+      galleryImages: (BLACK_AESTHETIC_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "4",
+      name: "Minimal Mood",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (MINIMAL_MOOD_IMAGES.coverImage),
+      galleryImages: (MINIMAL_MOOD_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "5",
+      name: "Rose Love",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (ROSE_LOVE_IMAGES.coverImage),
+      galleryImages: (ROSE_LOVE_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "6",
+      name: "Elegant Inspo",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (ELEGANT_INSPO_IMAGES.coverImage),
+      galleryImages: (ELEGANT_INSPO_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "7",
+      name: "Green Experiences",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (GREEN_EXPERIENCES_IMAGES.coverImage),
+      galleryImages: (GREEN_EXPERIENCES_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "8",
+      name: "Ocean Blue",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (OCEAN_BLUE_IMAGES.coverImage),
+      galleryImages: (OCEAN_BLUE_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "9",
+      name: "Mix Nude",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (MIX_NUDE_IMAGES.coverImage),
+      galleryImages: (MIX_NUDE_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "10",
+      name: "Sol Grey",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (SOL_GREY_IMAGES.coverImage),
+      galleryImages: (SOL_GREY_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "11",
+      name: "Curve Pink",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (CURVE_PINK_IMAGES.coverImage),
+      galleryImages: (CURVE_PINK_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "12",
+      name: "Green World",
+      type: "Plantillas Instagram",
+      price: { ARS: "6000", USD: "30", EUR: "28" },
+      coverImage: (GREEN_WORLD_IMAGES.coverImage),
+      galleryImages: (GREEN_WORLD_IMAGES.galleryImages),
+      content: contentTemplates
+    },
+    {
+      id: "13",
+      name: "Three Pack 01",
+      type: "Pack Plantillas",
+      price: { ARS: "13000", USD: "65", EUR: "60" },
+      coverImage: (THREE_PACK_01_IMAGES.coverImage),
+      galleryImages: (THREE_PACK_01_IMAGES.galleryImages),
+      content: [
+        "Paquete de plantillas Aesthetic Brown",
+        "Paquete de plantillas Minimal Mood",
+        "Paquete de plantillas Elegant Inspo"
+      ]
+    },
+    {
+      id: "14",
+      name: "Three Pack 02",
+      type: "Pack Plantillas",
+      price: { ARS: "13000", USD: "65", EUR: "60" },
+      coverImage: (THREE_PACK_02_IMAGES.coverImage),
+      galleryImages: (THREE_PACK_02_IMAGES.galleryImages),
+      content: [
+        "Paquete de plantillas Rose Love",
+        "Paquete de plantillas Mix Nude",
+        "Paquete de plantillas Curve Pink"
+      ]
+    },
+    {
+      id: "15",
+      name: "Three Pack 03",
+      type: "Pack Plantillas",
+      price: { ARS: "13000", USD: "65", EUR: "60" },
+      coverImage: (THREE_PACK_03_IMAGES.coverImage),
+      galleryImages: (THREE_PACK_03_IMAGES.galleryImages),
+      content: [
+        "Paquete de plantillas Black Aesthetic",
+        "Paquete de plantillas Neutral Minimalist",
+        "Paquete de plantillas Sol Grey"
+      ]
+    },
+    {
+      id: "16",
+      name: "Three Pack 04",
+      type: "Pack Plantillas",
+      price: { ARS: "13000", USD: "65", EUR: "60" },
+      coverImage: (THREE_PACK_04_IMAGES.coverImage),
+      galleryImages: (THREE_PACK_04_IMAGES.galleryImages),
+      content: [
+        "Paquete de plantillas Ocean Blue",
+        "Paquete de plantillas Green Experiences",
+        "Paquete de plantillas Green World"
+      ]
+    },
+    {
+      id: "17",
+      name: "Twelve Pack",
+      type: "Pack Plantillas",
+      price: { ARS: "50400", USD: "170", EUR: "158" },
+      coverImage: (TWELVE_PACK_IMAGES.coverImage),
+      galleryImages: (TWELVE_PACK_IMAGES.galleryImages),
+      content: [
+        "Incluye los 12 Packs de plantillas disponibles en nuestra tienda:",
+        "· Pack Aesthetic Brown",
+        "· Pack Neutral Minimalist",
+        "· Pack Black Aesthetic",
+        "· Pack Minimal Mood",
+        "· Pack Rose Love",
+        "· Pack Elegant Inspo",
+        "· Pack Green Experiences",
+        "· Pack Ocean Blue",
+        "· Pack Mix Nude",
+        "· Pack Sol Grey",
+        "· Pack Curve Pink",
+        "· Pack Green World",
+      ]
+    }
   ]
 }
